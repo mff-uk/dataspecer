f590f000667f37a5a1f24d8a286fd1d47c0bafd9
Štěpán Stenchlák <s.stenchlak@gmail.com>
2021-06-13 21:29:04 +0200

    PSM list is collapsable

--- src/components/psm/PsmAssociationClassItem.tsx
+++ src/components/psm/PsmAssociationClassItem.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import {PimClass, PsmAssociation, PsmAttribute, PsmClass} from "model-driven-data";
 import {PsmAttributeItem} from "./PsmAttributeItem";
 import {StoreContext} from "../App";
-import {Chip} from "@material-ui/core";
+import {Chip, Collapse} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import EditIcon from "@material-ui/icons/Edit";
 import {AddInterpretedSurroundingDialog} from "../addInterpretedSurroundings/addInterpretedSurroundingDialog";
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import {createStyles, makeStyles} from "@material-ui/core/styles";
 import {AssociationDetailDialog} from "../psmDetail/AssociationDetailDialog";
 import {PsmInterpretedAgainst} from "./PsmInterpretedAgainst";
 import {useToggle} from "../../hooks/useToggle";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 const termStyle: React.CSSProperties = {
     fontFamily: "monospace",
@@ -17,7 +19,7 @@ const termStyle: React.CSSProperties = {
     color: "red",
 };
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
     createStyles({
         root: {
             "&>div": {
@@ -29,6 +31,10 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         chip: {
             transition: "opacity 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms"
+        },
+        icon: {
+            verticalAlign: "middle",
+            cursor: "pointer",
         }
     }),
 );
@@ -49,11 +55,16 @@ export const PsmAssociationClassItem: React.FC<{id: string}> = ({id}) => {
 
     const surroundingDialog = useToggle();
     const editDialog = useToggle();
+    const collapse = useToggle(true);
 
     const styles = useStyles();
 
     return <li>
         <div className={styles.root}>
+            {collapse.isOpen ?
+                <ExpandMoreIcon className={styles.icon} onClick={collapse.close} /> :
+                <ExpandLessIcon className={styles.icon} onClick={collapse.open} />
+            }
             {association &&
                 <>
                     {association.psmTechnicalLabel ?
@@ -74,13 +85,15 @@ export const PsmAssociationClassItem: React.FC<{id: string}> = ({id}) => {
                 <Chip className={styles.chip} variant="outlined" size="small" onClick={editDialog.open} icon={<EditIcon/>} label={"edit"}/>
             </>}
         </div>
-        <ul>
-            {cls.psmParts?.map(part => {
-                if (PsmAttribute.is(store[part])) return <PsmAttributeItem id={part} key={part} />;
-                if (PsmAssociation.is(store[part])) return <PsmAssociationClassItem id={part} key={part} />;
-                return null;
-            })}
-        </ul>
+        <Collapse in={collapse.isOpen} unmountOnExit>
+            <ul>
+                {cls.psmParts?.map(part => {
+                    if (PsmAttribute.is(store[part])) return <PsmAttributeItem id={part} key={part} />;
+                    if (PsmAssociation.is(store[part])) return <PsmAssociationClassItem id={part} key={part} />;
+                    return null;
+                })}
+            </ul>
+        </Collapse>
 
         <AddInterpretedSurroundingDialog store={store} isOpen={surroundingDialog.isOpen} close={surroundingDialog.close} selected={psmSelectedInterpretedSurroundings} psmClass={cls} />
         {association && <AssociationDetailDialog store={store} association={association} isOpen={editDialog.isOpen} close={editDialog.close} updateTechnicalLabel={psmModifyTechnicalLabel} />}
--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -2,8 +2,8 @@ import {useCallback, useState} from "react";
 
 // https://github.com/opendata-mvcr/mission-control/blob/main/src/hooks/useToggle.ts
 
-export const useToggle = () => {
-    const [isOpen, setOpen] = useState(false);
+export const useToggle = (initialValue: boolean = false) => {
+    const [isOpen, setOpen] = useState(initialValue);
 
     const open = useCallback(() => {
         setOpen(true);
