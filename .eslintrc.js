4f65626726150abbb1608d3ef21f0ff0a8e4831c
Petr Škoda <skodapetr@gmail.com>
2021-06-23 13:09:23 +0200

    Update linter file

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "root": true,
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "es2021",
+    "sourceType": "module",
+  },
+  "rules": {
+    "quotes": ["error", "double"],
+    "indent": ["error", 2, {"SwitchCase": 1}],
+    "comma-dangle": ["error", "always-multiline"],
+    "linebreak-style": ["error", "unix"],
+    "semi": ["error", "always"],
+    // As of now the tests need manual inspection. This should be removed
+    // later when we have stable inputs thus we can prepare expected
+    // values for the tests.
+    "jest/expect-expect": "off",
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "jest",
+  ],
+  "env": {
+    "jest/globals": true,
+    "browser": true,
+    "es2021": true,
+    "node": true,
+  }
+};
