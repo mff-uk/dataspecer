6e335d40ddc5c8c9934ae7805eb9926b5d0e7669
Petr Škoda <skodapetr@gmail.com>
2021-06-23 18:27:36 +0200

    Lint file

--- lib/platform-model/platform-model-store.ts
+++ lib/platform-model/platform-model-store.ts
@@ -3,6 +3,6 @@ import {ModelResource} from "./platform-model-api";
 export type Store = Record<string, ModelResource>;
 
 export function mergeStore(...stores: Store[]): Store {
-    // todo: Implement properly. Merge instead of override
-    return stores.reduce((accumulator, current) => ({...accumulator, ...current}), {});
+  // TODO: Implement properly. Merge instead of override
+  return stores.reduce((accumulator, current) => ({...accumulator, ...current}), {});
 }
