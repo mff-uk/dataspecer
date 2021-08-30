dce6a2e95b3f71b1ffb16189a24e741529c68908
Petr Škoda <skodapetr@gmail.com>
2021-08-30 08:47:59 +0200

    Update bikeshed
    Typo and adapter function name.

--- lib/bikeshed/bikeshed-model-adapter.ts
+++ lib/bikeshed/bikeshed-model-adapter.ts
@@ -2,25 +2,25 @@ import {ObjectModelSchema} from "../object-model";
 import {Bikeshed, BikeshedMetadataKeys} from "./bikeshed-model";
 import {
   defaultStringSelector,
-  createWebSpecificationFromObjectModel,
+  objectModelToWebSpecification,
   defaultRootSelector,
   DefaultLinkFactory,
   WebSpecification,
 } from "../web-specification";
 
-export function schemaAsBikeshed(schema: ObjectModelSchema): Bikeshed {
+export function objectModelToBikeshed(schema: ObjectModelSchema): Bikeshed {
   const result = new Bikeshed();
-  const specification = createWebSpecificationFromObjectModel(
+  const specification = objectModelToWebSpecification(
     schema, defaultRootSelector, defaultStringSelector,
     new DefaultLinkFactory(schema));
   result.humanLabel = specification.humanLabel;
   result.humanDescription = specification.humanDescription;
   result.schemas = specification.schemas;
-  result.metadata = loadBikehedMetadata(schema, specification);
+  result.metadata = loadBikeshedMetadata(schema, specification);
   return result;
 }
 
-function loadBikehedMetadata(
+function loadBikeshedMetadata(
   schema: ObjectModelSchema, specification: WebSpecification
 ): Record<string, string> {
   return {
