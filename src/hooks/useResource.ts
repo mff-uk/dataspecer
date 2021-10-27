515db9921d8f9fbd1de45093f62057036c2feba5
Štěpán Stenchlák <s.stenchlak@gmail.com>
2021-10-27 10:34:47 +0200

    Optimize useResource hook

--- src/hooks/useResource.ts
+++ src/hooks/useResource.ts
@@ -6,12 +6,30 @@ import {Subscriber} from "../store/observable-core-resource-reader-writer";
 
 export const useResource = <ResourceType extends CoreResource>(iri: string | null) => {
     const {store} = React.useContext(StoreContext);
-    const [state, setState] = useState<CoreResourceLink<ResourceType>>({
-        resource: null,
-        isLoading: true,
+    const [state, setState] = useState<CoreResourceLink<ResourceType>>(() => {
+        if (iri) {
+            const cached = store.optimizeGetCachedValue(iri) as CoreResourceLink<ResourceType>;
+            if (cached) {
+                return cached;
+            }
+        }
+
+        return {
+            resource: null,
+            isLoading: true,
+        }
     });
 
-    const subscriber = useCallback<Subscriber>((_, resource) => setState(resource as CoreResourceLink<ResourceType>), []);
+    const [stateInjector] = useState({state});
+    stateInjector.state = state;
+
+    const subscriber = useCallback<Subscriber>((_, resource) => {
+        const state = stateInjector.state;
+        if (resource.resource === state.resource && resource.isLoading === state.isLoading) {
+            return;
+        }
+        setState(resource as CoreResourceLink<ResourceType>);
+    }, []);
 
     useEffect(() => {
         if (iri) {
