62aa462ea9b298bb4afe74d12bf4c169211f9abb
Štěpán Stenchlák <s.stenchlak@gmail.com>
2021-10-27 10:36:20 +0200

    i18n improvement logic

---
+++ i18next-parser.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    locales: ['en', 'cs'],
+    output: 'locales/$LOCALE/$NAMESPACE.json',
+    input: 'src/**/*.{ts,tsx}',
+    sort: true,
+}
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,29 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import cs from './i18n/cs.json';
-import en from './i18n/en.json';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-const languages = ['cs', 'en'];
+const context = require.context('../locales', true, /\.json$/);
+const translations: {
+    [locale: string]: {
+        [namespace: string]: {}
+    }
+} = {};
+
+for (const filename of context.keys()) {
+    const [locale, namespace] = filename.substr(2, filename.length - 2 - 5).split("/"); // ./locale/namespace.json
+    if (!translations[locale]) {
+        translations[locale] = {};
+    }
+    translations[locale][namespace] = context(filename);
+}
+
+const languages = Object.keys(translations);
 
 i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        resources: {cs, en},
+        resources: translations,
         fallbackLng: 'en',
         whitelist: languages,
 
