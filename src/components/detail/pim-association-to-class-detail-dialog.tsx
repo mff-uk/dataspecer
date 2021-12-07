73504cd221ae92372ee7823ff504b92ad4c4514e
Štěpán Stenchlák <s.stechlak@gmail.com>
2021-12-07 17:31:04 +0100

    Fix detail dialog missing store

--- src/components/detail/pim-association-to-class-detail-dialog.tsx
+++ src/components/detail/pim-association-to-class-detail-dialog.tsx
@@ -1,5 +1,5 @@
 import React, {memo} from "react";
-import {DialogContentText, Grid} from "@mui/material";
+import {DialogContentText, Grid, Tab, Tabs} from "@mui/material";
 import {LanguageStringFallback} from "../helper/LanguageStringComponents";
 import {useResource} from "../../hooks/useResource";
 import {PimAssociation, PimClass} from "model-driven-data/pim/model";
@@ -9,6 +9,7 @@ import {InDifferentLanguages} from "./components/InDifferentLanguages";
 import {CimLinks} from "./components/cim-links";
 import {dialog, DialogParameters} from "../../dialog";
 import {DialogWrapper} from "./common";
+import {ResourceInStore} from "./components/resource-in-store";
 
 export const PimAssociationToClassDetailDialog: React.FC<{parentIri: string, iri: string, orientation: boolean} & DialogParameters> = dialog({maxWidth: "lg", fullWidth: true}, memo(({iri, orientation, close}) => {
     const {resource: association} = useResource<PimAssociation>(iri);
@@ -16,6 +17,10 @@ export const PimAssociationToClassDetailDialog: React.FC<{parentIri: string, iri
 
     let childIri: string | null = association?.pimEnd[orientation ? 1 : 0] ?? null;
     const {resource: child} = useResource<PimClass>(childIri);
+
+    const [tab, setTab] = React.useState(0);
+    const [storeTab, setStoreTab] = React.useState("pimAssociation");
+
 
     return <DialogWrapper close={close} title={<>
         <div>
@@ -38,19 +43,35 @@ export const PimAssociationToClassDetailDialog: React.FC<{parentIri: string, iri
             <LanguageStringFallback from={child?.pimHumanDescription ?? {}} fallback={<i>{t("no description")}</i>}/>
         </DialogContentText>
     </>}>
-        <Grid container spacing={5} sx={{pt: 3}}>
-            <Grid item xs={6}>
-                <InDifferentLanguages
-                    label={association?.pimHumanLabel ?? {}}
-                    description={association?.pimHumanDescription ?? {}}
-                />
+        <Tabs centered value={tab} onChange={(e, ch) => setTab(ch)}>
+            <Tab label={t('tab basic info')} />
+            <Tab label={t('tab store')} />
+        </Tabs>
+
+        {tab === 0 &&
+            <Grid container spacing={5} sx={{pt: 3}}>
+                <Grid item xs={6}>
+                    <InDifferentLanguages
+                        label={association?.pimHumanLabel ?? {}}
+                        description={association?.pimHumanDescription ?? {}}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    {childIri && <InDifferentLanguages
+                        label={child?.pimHumanLabel ?? {}}
+                        description={child?.pimHumanDescription ?? {}}
+                    />}
+                </Grid>
             </Grid>
-            <Grid item xs={6}>
-                {childIri && <InDifferentLanguages
-                    label={child?.pimHumanLabel ?? {}}
-                    description={child?.pimHumanDescription ?? {}}
-                />}
-            </Grid>
-        </Grid>
+        }
+        {tab === 1 && <>
+            <Tabs value={storeTab} onChange={(e, ch) => setStoreTab(ch)} sx={{ mb: 3 }}>
+                <Tab label={t('tab pim association')} value={"pimAssociation"} />
+                <Tab label={t('tab pim child')} value={"pimChild"} />
+            </Tabs>
+
+            {storeTab === "pimAssociation" && <ResourceInStore iri={iri} />}
+            {storeTab === "pimChild" && childIri && <ResourceInStore iri={childIri} />}
+        </>}
     </DialogWrapper>;
 }));
