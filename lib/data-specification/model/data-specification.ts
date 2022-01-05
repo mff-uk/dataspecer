cf9bb77a9f2e6ad74a7285ec9bdd2dba4c6329ff
Petr Škoda <skodapetr@gmail.com>
2022-01-05 11:46:25 +0100

    Name is loaded from PIM

--- lib/data-specification/model/data-specification.ts
+++ lib/data-specification/model/data-specification.ts
@@ -4,11 +4,6 @@ export class DataSpecification {
 
   iri: string | null = null;
 
-  /**
-   * User given name, we do not support multiple languages here.
-   */
-  name: string | null = null;
-
   pim: string | null = null;
 
   psms: string [] = [];
