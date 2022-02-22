0244472467a625ceab985b8878bca1be15adc197
Jan Janda <janjanda42@gmail.com>
2022-02-22 17:15:42 +0100

    Prettify code

--- packages/core/src/generator/generator-factory.ts
+++ packages/core/src/generator/generator-factory.ts
@@ -1,11 +1,11 @@
-import {DataSpecification} from "../data-specification/model";
-import {CoreResourceReader} from "../core";
-import {Generator} from "./generator";
-import {BikeshedGenerator} from "../bikeshed";
-import {JsonSchemaGenerator} from "../json-schema/json-schema-generator";
-import {XmlSchemaGenerator} from "../xml-schema/xml-schema-generator";
-import {CsvSchemaGenerator} from "../csv-schema/csv-schema-generator";
-import {PlantUmlGenerator} from "../plant-uml";
+import { DataSpecification } from "../data-specification/model";
+import { CoreResourceReader } from "../core";
+import { Generator } from "./generator";
+import { BikeshedGenerator } from "../bikeshed";
+import { JsonSchemaGenerator } from "../json-schema/json-schema-generator";
+import { XmlSchemaGenerator } from "../xml-schema/xml-schema-generator";
+import { CsvSchemaGenerator } from "../csv-schema/csv-schema-generator";
+import { PlantUmlGenerator } from "../plant-uml";
 
 /**
  * Use this to get generator with included artefact generators from this
@@ -15,12 +15,11 @@ export function createDefaultGenerator(
   specifications: DataSpecification[],
   reader: CoreResourceReader,
 ): Generator {
-  return new Generator(specifications, reader,
-    [
-      new BikeshedGenerator(),
-      new JsonSchemaGenerator(),
-      new XmlSchemaGenerator(),
-      new CsvSchemaGenerator(),
-      new PlantUmlGenerator(),
-    ]);
+  return new Generator(specifications, reader, [
+    new BikeshedGenerator(),
+    new JsonSchemaGenerator(),
+    new XmlSchemaGenerator(),
+    new CsvSchemaGenerator(),
+    new PlantUmlGenerator(),
+  ]);
 }
