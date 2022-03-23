3c4b36ccf876e03897fdee7a62f92045780684e6
Štěpán Stenchlák <s.stenchlak@gmail.com>
2022-03-23 15:48:39 +0100

    Prisma database migration

--- services/backend/prisma/migrations/20220223204021_refactoring/migration.sql
+++ services/backend/prisma/migrations/20220223204021_refactoring/migration.sql
@@ -1,18 +1,5 @@
 /*
-  Warnings:
-
-  - You are about to drop the column `name` on the `DataSpecification` table. All the data in the column will be lost.
-  - You are about to drop the column `pimStore` on the `DataSpecification` table. All the data in the column will be lost.
-  - You are about to drop the column `DataSpecificationId` on the `DataStructure` table. All the data in the column will be lost.
-  - You are about to drop the column `artifact_json` on the `DataStructure` table. All the data in the column will be lost.
-  - You are about to drop the column `artifact_xml` on the `DataStructure` table. All the data in the column will be lost.
-  - You are about to drop the column `name` on the `DataStructure` table. All the data in the column will be lost.
-  - You are about to drop the column `store` on the `DataStructure` table. All the data in the column will be lost.
-  - Added the required column `pimSchema` to the `DataSpecification` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `storeId` to the `DataSpecification` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `psmSchema` to the `DataStructure` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `storeId` to the `DataStructure` table without a default value. This is not possible if the table is not empty.
-
+    Manual migration file
 */
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
@@ -21,9 +8,11 @@ CREATE TABLE "new_DataSpecification" (
     "pimSchema" TEXT NOT NULL,
     "storeId" TEXT NOT NULL
 );
-INSERT INTO "new_DataSpecification" ("id") SELECT "id" FROM "DataSpecification";
+INSERT INTO "new_DataSpecification" ("id", "pimSchema", "storeId") SELECT "https://ofn.gov.cz/data-specification/" || id, "INVALID:" || id, pimStore FROM "DataSpecification";
 DROP TABLE "DataSpecification";
 ALTER TABLE "new_DataSpecification" RENAME TO "DataSpecification";
+
+
 CREATE UNIQUE INDEX "DataSpecification_pimSchema_key" ON "DataSpecification"("pimSchema");
 CREATE UNIQUE INDEX "DataSpecification_storeId_key" ON "DataSpecification"("storeId");
 CREATE TABLE "new_DataStructure" (
@@ -33,9 +22,17 @@ CREATE TABLE "new_DataStructure" (
     "belongsToDataSpecificationId" TEXT,
     CONSTRAINT "DataStructure_belongsToDataSpecificationId_fkey" FOREIGN KEY ("belongsToDataSpecificationId") REFERENCES "DataSpecification" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-INSERT INTO "new_DataStructure" ("id") SELECT "id" FROM "DataStructure";
+INSERT INTO "new_DataStructure" ("id", "storeId", psmSchema, belongsToDataSpecificationId) SELECT id, store, "INVALID:" || id, "https://ofn.gov.cz/data-specification/" || DataSpecificationId FROM "DataStructure";
 DROP TABLE "DataStructure";
 ALTER TABLE "new_DataStructure" RENAME TO "DataStructure";
 CREATE UNIQUE INDEX "DataStructure_psmSchema_key" ON "DataStructure"("psmSchema");
+
+UPDATE "_DataSpecificationReuse"
+SET
+A = "https://ofn.gov.cz/data-specification/" || B,
+B = "https://ofn.gov.cz/data-specification/" || A;
+
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
+
+
