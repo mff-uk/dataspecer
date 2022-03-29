2bd878acb2550e7189c12c93907cd3187a0dd690
Jan Janda <janjanda42@gmail.com>
2022-03-29 18:32:10 +0200

    Add all types to CSV adapter

--- packages/core/src/csv-schema/csv-schema-model-adapter.ts
+++ packages/core/src/csv-schema/csv-schema-model-adapter.ts
@@ -5,11 +5,11 @@ import {
 } from "./csv-schema-model";
 import {
     StructureModel,
-    StructureModelPrimitiveType
+    StructureModelPrimitiveType, StructureModelProperty
 } from "../structure-model";
 import { DataSpecification } from "../data-specification/model";
 import { assert } from "../core";
-import {OFN} from "../well-known";
+import { OFN } from "../well-known";
 
 export function structureModelToCsvSchema(
     specification: DataSpecification,
@@ -18,8 +18,28 @@ export function structureModelToCsvSchema(
     assert(model.roots.length === 1, "Exactly one root class must be provided.");
     const schema = new CsvSchema();
     schema["@id"] = "https://ofn.gov.cz/schema/" + specification.artefacts[2].publicUrl;
-    schema.tableSchema = new TableSchema();
-    for (const prop of model.classes[model.roots[0]].properties) {
+    schema.tableSchema = createTableSchemaFromProperties(model.classes[model.roots[0]].properties);
+
+    // adds rdf:type virtual column
+    const virtualCol = new Column();
+    virtualCol.virtual = true;
+    virtualCol.propertyUrl = "rdf:type";
+    virtualCol.valueUrl = model.classes[model.roots[0]].cimIri;
+    schema.tableSchema.columns.push(virtualCol);
+
+    return schema;
+}
+
+/**
+ * This function creates columns of table schema according to provided properties.
+ * @param properties Properties of root class in structure model
+ * @returns Table schema with columns in it
+ */
+function createTableSchemaFromProperties (
+    properties: StructureModelProperty[]
+) : TableSchema {
+    const tableSchema = new TableSchema();
+    for (const prop of properties) {
         const col = new Column();
         col.name = prop.technicalLabel;
         col.titles = prop.technicalLabel;
@@ -30,16 +50,16 @@ export function structureModelToCsvSchema(
         if(dataType.isAttribute()) col.datatype = structureModelPrimitiveToCsvDefinition(dataType);
 
         col.lang = "cs";
-        schema.tableSchema.columns.push(col);
+        tableSchema.columns.push(col);
     }
-    const virtualCol = new Column();
-    virtualCol.virtual = true;
-    virtualCol.propertyUrl = "rdf:type";
-    virtualCol.valueUrl = model.classes[model.roots[0]].cimIri;
-    schema.tableSchema.columns.push(virtualCol);
-    return schema;
+    return tableSchema;
 }
 
+/**
+ * This function translates primitive types from structure model to CSVW types according to https://www.w3.org/TR/tabular-metadata/#datatypes
+ * @param primitive Primitive type from structure model
+ * @returns String name of the translated datatype or null if not applicable
+ */
 function structureModelPrimitiveToCsvDefinition(
     primitive: StructureModelPrimitiveType
 ) : string | null {
@@ -51,14 +71,26 @@ function structureModelPrimitiveToCsvDefinition(
         case OFN.date:
             result = "date";
             break;
+        case OFN.time:
+            result = "time";
+            break;
+        case OFN.dateTime:
+            result = "dateTime";
+            break;
+        case OFN.integer:
+            result = "integer";
+            break;
+        case OFN.decimal:
+            result = "decimal";
+            break;
+        case OFN.url:
+            result = "anyURI";
+            break;
         case OFN.string:
             result = "string";
             break;
         case OFN.text:
             result = "string";
-            break;
-        case OFN.integer:
-            result = "integer";
             break;
     }
     return result;
