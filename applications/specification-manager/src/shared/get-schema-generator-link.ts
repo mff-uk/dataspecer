ac22a01176a7a67d7cb1a3502d2e68866889e750
Štěpán Stenchlák <s.stenchlak@gmail.com>
2022-04-06 12:44:10 +0200

    Allow relative URLs in REACT_APP_SCHEMA_GENERATOR

--- applications/specification-manager/src/shared/get-schema-generator-link.ts
+++ applications/specification-manager/src/shared/get-schema-generator-link.ts
@@ -1,5 +1,5 @@
 export function getSchemaGeneratorLink(dataSpecificationIri: string, dataPsmSchemaIri: string) {
-    const editSchemaGeneratorUrl = new URL(process.env.REACT_APP_SCHEMA_GENERATOR as string);
+    const editSchemaGeneratorUrl = new URL(process.env.REACT_APP_SCHEMA_GENERATOR as string, window.location.href);
     editSchemaGeneratorUrl.searchParams.append('data-specification', dataSpecificationIri);
     editSchemaGeneratorUrl.searchParams.append('data-psm-schema', dataPsmSchemaIri);
     editSchemaGeneratorUrl.searchParams.append('backlink', window.location.href);
