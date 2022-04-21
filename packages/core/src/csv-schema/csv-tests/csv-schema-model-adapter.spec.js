17adef146f59347b7e80ac0a7d215ff51e369901
Jan Janda <janjanda42@gmail.com>
2022-04-21 11:14:33 +0200

    Refactor CSV tests

--- packages/core/src/csv-schema/csv-tests/csv-schema-model-adapter.spec.js
+++ packages/core/src/csv-schema/csv-tests/csv-schema-model-adapter.spec.js
@@ -1,9 +1,11 @@
-import * as resources from "./test-resources";
+import * as resources from "./csv-test-resources";
 import { ReadOnlyMemoryStore } from "../../core";
 import { coreResourcesToConceptualModel } from "../../conceptual-model";
 import { coreResourcesToStructuralModel } from "../../structure-model";
 import { transformStructureModel } from "../../structure-model/transformation";
 import { structureModelToCsvSchema } from "../csv-schema-model-adapter";
+
+const testNamePrefix = "CSV generator: ";
 
 async function arrangeSpecAndModel(storeResource, specificationResource) {
     const dataSpecificationIri = Object.values(specificationResource)[0].iri;
@@ -30,13 +32,13 @@ async function arrangeSpecAndModel(storeResource, specificationResource) {
     return { dataSpecification, structureModel };
 }
 
-test("correct @id", async () => {
+test(testNamePrefix + "correct @id", async () => {
     const arranged = await arrangeSpecAndModel(resources.storeResource_1, resources.specificationResource_1);
     const result = structureModelToCsvSchema(arranged.dataSpecification, arranged.structureModel);
     expect(result["@id"]).toBe("https://ofn.gov.cz/schema/testjj/jazyk/schema.csv-metadata.json");
 });
 
-test("rdf::type column", async () => {
+test(testNamePrefix + "rdf::type column", async () => {
     const arranged = await arrangeSpecAndModel(resources.storeResource_1, resources.specificationResource_1);
     const result = structureModelToCsvSchema(arranged.dataSpecification, arranged.structureModel);
     expect(result.tableSchema.columns[result.tableSchema.columns.length - 1].propertyUrl).toBe("rdf:type");
