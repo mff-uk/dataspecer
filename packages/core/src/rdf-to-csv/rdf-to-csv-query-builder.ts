87304896646b415eb22394d2fe95a2d49d273769
Jan Janda <janjanda42@gmail.com>
2022-08-06 11:45:26 +0200

    Add function for prefixes in RDF to CSV

--- packages/core/src/rdf-to-csv/rdf-to-csv-query-builder.ts
+++ packages/core/src/rdf-to-csv/rdf-to-csv-query-builder.ts
@@ -38,9 +38,54 @@ function processColumn(
 
 const csvwContext = JSON.parse(readFileSync(join(__dirname, "csvw-context.jsonld"), "utf8"));
 
+/**
+ * This function uses CSVW context to resolve prefix and create a full IRI from a compact IRI.
+ */
 export function resolveCompactIri(compact: CompactIRI) : string {
     let absolute = csvwContext["@context"][compact.prefix];
     if (absolute === undefined) assertFailed("Undefined prefix!");
     absolute += compact.suffix;
     return absolute;
+}
+
+/**
+ * This function splits full absolute IRI into a namespace and a local part.
+ */
+export function splitIri(fullIri: string) : { namespace: string, local: string} {
+    let lastSlash = 0;
+    for (let i = 0; i < fullIri.length; i++) {
+        if (fullIri[i] === "/") lastSlash = i;
+    }
+    return { namespace: fullIri.slice(0, lastSlash + 1), local: fullIri.slice(lastSlash + 1) }
+}
+
+/**
+ * This function creates a prefix from a namespace IRI, adds the namespace into query and returns the prefix.
+ */
+export function addPrefix(namespaceIri: string, query: SparqlSelectQuery) : string {
+    // Check if the namespace is already present.
+    for (const ns in query.prefixes) {
+        if (query.prefixes[ns] === namespaceIri) return ns;
+    }
+
+    // Check if the namespace is well-known.
+    for (const key in csvwContext["@context"]) {
+        if (csvwContext["@context"][key] === namespaceIri) {
+            query.prefixes[key] = namespaceIri;
+            return key;
+        }
+    }
+
+    // Find max number of generic prefix.
+    const genericPrefix = "ns";
+    let max = 0;
+    for (const ns in query.prefixes) {
+        if (ns.slice(0, 2) === genericPrefix) {
+            let nsNumber = parseInt(ns.slice(2));
+            if (nsNumber > max) max = nsNumber;
+        }
+    }
+    const newPrefix = genericPrefix + (max + 1).toString();
+    query.prefixes[newPrefix] = namespaceIri;
+    return newPrefix;
 }
--- packages/core/src/rdf-to-csv/tests/rdf-to-csv-query-builder.spec.js
+++ packages/core/src/rdf-to-csv/tests/rdf-to-csv-query-builder.spec.js
@@ -1,9 +1,12 @@
 import { createCsvSchema } from "../../csv-schema/tests/test-helpers";
 import {
     buildQuery,
-    resolveCompactIri
+    resolveCompactIri,
+    splitIri,
+    addPrefix
 } from "../rdf-to-csv-query-builder";
 import { CompactIRI } from "../../csv-schema/csv-schema-model";
+import { SparqlSelectQuery } from "../../sparql-query/sparql-model";
 
 const testNamePrefix = "RDF to CSV: ";
 
@@ -24,4 +27,61 @@ test(testNamePrefix + "resolve compact IRI", async () => {
 test(testNamePrefix + "resolve invalid compact IRI", async () => {
     const compact = new CompactIRI("qwert", "type");
     expect(() => resolveCompactIri(compact)).toThrow();
+});
+
+test(testNamePrefix + "split IRI namespace", async () => {
+    const split = splitIri("https://slovník.gov.cz/datový/číselníky/pojem/alternativní-název-položky-číselníku");
+    expect(split.namespace).toBe("https://slovník.gov.cz/datový/číselníky/pojem/");
+});
+
+test(testNamePrefix + "split IRI local", async () => {
+    const split = splitIri("https://slovník.gov.cz/datový/číselníky/pojem/alternativní-název-položky-číselníku");
+    expect(split.local).toBe("alternativní-název-položky-číselníku");
+});
+
+test(testNamePrefix + "add prefix", async () => {
+    const query = new SparqlSelectQuery();
+    query.prefixes = {};
+    const newPrefix = addPrefix("https://slovník.gov.cz/datový/číselníky/pojem/", query);
+    expect(newPrefix).toBe("ns1");
+});
+
+test(testNamePrefix + "add prefixes", async () => {
+    const query = new SparqlSelectQuery();
+    query.prefixes = {};
+    addPrefix("https://slovník.gov.cz/datový/číselníky/dojem/", query);
+    addPrefix("https://slovník.gov.cz/datový/číselníky/hnojem/", query);
+    const newPrefix = addPrefix("https://slovník.gov.cz/datový/číselníky/pojem/", query);
+    expect(newPrefix).toBe("ns3");
+});
+
+test(testNamePrefix + "add same prefixes", async () => {
+    const query = new SparqlSelectQuery();
+    query.prefixes = {};
+    addPrefix("https://slovník.gov.cz/datový/číselníky/dojem/", query);
+    addPrefix("https://slovník.gov.cz/datový/číselníky/pojem/", query);
+    const newPrefix = addPrefix("https://slovník.gov.cz/datový/číselníky/dojem/", query);
+    expect(newPrefix).toBe("ns1");
+});
+
+test(testNamePrefix + "add well-known prefix", async () => {
+    const query = new SparqlSelectQuery();
+    query.prefixes = {};
+    const newPrefix = addPrefix("http://www.w3.org/1999/02/22-rdf-syntax-ns#", query);
+    expect(newPrefix).toBe("rdf");
+});
+
+test(testNamePrefix + "add well-known prefixes", async () => {
+    const query = new SparqlSelectQuery();
+    query.prefixes = {};
+    addPrefix("http://www.w3.org/1999/02/22-rdf-syntax-ns#", query);
+    const newPrefix = addPrefix("http://purl.org/dc/terms/", query);
+    expect(newPrefix).toBe("dc");
+});
+
+test(testNamePrefix + "check if well-known prefix saved", async () => {
+    const query = new SparqlSelectQuery();
+    query.prefixes = {};
+    addPrefix("http://www.w3.org/1999/02/22-rdf-syntax-ns#", query);
+    expect(query.prefixes["rdf"]).toBe("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 });
