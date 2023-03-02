b32219e7656d735b86f5db7360d73f025e9f9b99
Štěpán Stenchlák <s.stenchlak@gmail.com>
2023-03-03 00:30:56 +0100

    Fix cloudflare build

--- cloudflare.build.sh
+++ cloudflare.build.sh
@@ -4,8 +4,8 @@ npm install
 
 printf "REACT_APP_BACKEND=$BACKEND\nREACT_APP_DEBUG_VERSION=$CF_PAGES_BRANCH@$(echo $CF_PAGES_COMMIT_SHA | head -c7) $(date -u +%F\ %H:%M:%S)" > applications/client/.env.local
 
-lerna bootstrap --scope client --include-dependencies
-lerna run build --scope client --include-dependencies
+npx lerna bootstrap --scope client --include-dependencies
+npx lerna run build --scope client --include-dependencies
 
 rm -rf .dist
 mkdir .dist
