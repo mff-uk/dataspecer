a4ecdc8cd7d5d922f4c2c06830bc790405f3cff9
Štěpán Stenchlák <s.stenchlak@gmail.com>
2023-04-08 16:30:58 +0200

    Update Cloudflare build script

--- cloudflare.build.sh
+++ cloudflare.build.sh
@@ -4,10 +4,15 @@ npm install
 
 printf "REACT_APP_BACKEND=$BACKEND\nREACT_APP_DEBUG_VERSION=$CF_PAGES_BRANCH@$(echo $CF_PAGES_COMMIT_SHA | head -c7) $(date -u +%F\ %H:%M:%S)" > applications/client/.env.local
 
-npx lerna bootstrap --scope client --include-dependencies
-npx lerna run build --scope client --include-dependencies
+printf "NEXT_PUBLIC_BASE_PATH=/conceptual-model-editor" > applications/conceptual-model-editor/.env
+
+npx lerna bootstrap --scope client --scope conceptual-model-editor --include-dependencies
+npx lerna run build --scope client --scope conceptual-model-editor --include-dependencies
 
 rm -rf .dist
 mkdir .dist
 
 cp -r applications/client/build/* .dist
+
+mkdir .dist/conceptual-model-editor
+cp -r applications/conceptual-model-editor/out/* .dist/conceptual-model-editor
