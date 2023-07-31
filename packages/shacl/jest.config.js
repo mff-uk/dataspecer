34e46176ed956ea6c32d89bb8a345221a7295d1c
Tereza Miklóšová <t.miklosova@seznam.cz>
2023-08-01 01:33:21 +0200

    Commit revert to add only package changes
    This reverts commit 19b618bee8d31f36170e903d5522e12a4b9e0830.

--- packages/shacl/jest.config.js
+++ packages/shacl/jest.config.js
@@ -1,26 +1,12 @@
-const {defaults} = require("jest-config");
-// We could print default options here.
-
-const esModules = [
-  "rdf-ext",
-  "@rdfjs", // All libraries in this.
-  "nodeify-fetch",
-  "node-fetch",
-  "data-uri-to-buffer",
-  "fetch-blob",
-  "formdata-polyfill",
-  "is-stream",
-  "duplex-to"
-].join('|');
-const ignorePatterns =  [`/node_modules/(?!${esModules})`];
 module.exports = {
   "verbose": true,
   "moduleFileExtensions": [
     "js",
     "ts",
   ],
-  transformIgnorePatterns: ignorePatterns,
-  //"transform": {"^.+\\.[t|j]sx?$": "ts-jest" },
+  "transform": {
+    "^.+\\.[t|j]sx?$": "ts-jest"
+  },
   "roots": [
     "src",
   ],
