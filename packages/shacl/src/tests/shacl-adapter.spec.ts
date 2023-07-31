4d98f6f8d3e54fc55eafb4bcbbdc7e138a41f560
Tereza Miklóšová <t.miklosova@seznam.cz>
2023-08-01 01:57:16 +0200

    Config changes to support test runs

--- packages/shacl/src/tests/shacl-adapter.spec.ts
+++ packages/shacl/src/tests/shacl-adapter.spec.ts
@@ -1,38 +1,73 @@
 import { ShaclAdapter } from "../shacl-adapter";
-import * as fs from "fs";
-import { factory } from "rdf-ext";
-import { ParserN3 } from "@rdfjs/parser-n3";
-import { SHACLValidator } from "rdf-validate-shacl";
 
+import fs from "fs";
+import factory  from "rdf-ext";
+import  ParserN3  from "@rdfjs/parser-n3";
+import  SHACLValidator  from "rdf-validate-shacl";
 
+import {
+  StructureModel,
+  StructureModelClass,
+  StructureModelType,
+  StructureModelComplexType,
+  StructureModelProperty,
+  StructureModelPrimitiveType,
+  StructureModelCustomType,
+  StructureModelSchemaRoot,
+} from "@dataspecer/core/structure-model/model";
 
 const sum = new ShaclAdapter(null, null, null);
-
-/*
-const fs = require('fs');
-const factory = require('rdf-ext');
-const ParserN3 = require('@rdfjs/parser-n3');
-const SHACLValidator = require('rdf-validate-shacl');
-*/
-
-
-
-
+var validationResult : boolean;
 async function loadDataset (filePath) {
   const stream = fs.createReadStream(filePath)
   const parser = new ParserN3({ factory })
   return factory.dataset().import(parser.import(stream))
 }
 
+function createModel(): StructureModel{
+  var model = new StructureModel();
+
+    var primitiveType1 : StructureModelPrimitiveType;
+    primitiveType1 = new StructureModelPrimitiveType();
+
+    var property1 : StructureModelProperty;
+    property1 = new StructureModelProperty();
+    property1.cardinalityMax = 2;
+    property1.cardinalityMin = 0;
+    property1.cimIri = "https://example.com/mojeCimIri";
+    property1.dataTypes = [primitiveType1];
+    property1.dematerialize = false;
+    property1.humanDescription = {["cs"]: "Popisek 1"};
+    property1.humanLabel = {["cs"]: "Label 1"};
+    property1.isReverse = false;
+    property1.pathToOrigin = null;
+    property1.pimIri = "https://example.com/mojePimIri";
+    property1.psmIri = "https://example.com/mojePsmIri";
+    property1.technicalLabel = "technicky popisek";
+
+
+    var class1 : StructureModelClass;
+    class1 = new StructureModelClass();
+
+    var root1 : StructureModelSchemaRoot;
+    root1 = new StructureModelSchemaRoot();
+    root1.classes = [class1];
+
+    model.roots = [root1];
+
+  return model;
+}
+
 async function main() {
-  const shapes = await loadDataset('my-shapes.ttl')
-  const data = await loadDataset('my-data.ttl')
+  const shapes = await loadDataset('src/tests/shape.trig')
+  const data = await loadDataset('src/tests/data.trig')
 
   const validator = new SHACLValidator(shapes, { factory })
   const report = await validator.validate(data)
 
   // Check conformance: `true` or `false`
   console.log(report.conforms)
+  validationResult = report.conforms;
 
   for (const result of report.results) {
     // See https://www.w3.org/TR/shacl/#results-validation-result for details
@@ -46,13 +81,12 @@ async function main() {
   }
 
   // Validation report as RDF dataset
-  console.log(report.dataset)
+  //console.log(report.dataset)
 }
 
 main();
 
-/*
-test('adds 1 + 2 to equal 3', () => {
-  expect((1, 2)).toBe(3);
+test('Test SHACL ', async () => {
+  await main();
+  expect(validationResult).toBe(true);
 });
-*/
