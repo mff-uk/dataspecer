cb821031891d8ff76d0d120293c87db2e19b9105
Štěpán Stenchlák <s.stenchlak@gmail.com>
2023-10-10 21:55:59 +0200

    Fix flatten inheritance repeated run

--- packages/core/src/structure-model/transformation/instantiate-properties.ts
+++ packages/core/src/structure-model/transformation/instantiate-properties.ts
@@ -1,4 +1,4 @@
-import {StructureModel, StructureModelClass,} from "../model";
+import {StructureModel, StructureModelClass, StructureModelProperty,} from "../model";
 import {clone} from "../../core";
 
 /**
@@ -10,9 +10,18 @@ export function structureModelFlattenInheritance(
 ): StructureModel {
   const result = clone(structure) as StructureModel;
   const classes = result.getClasses();
+  // First traverse everything and then add properties to not end up in a cycle.
+  const propertiesToAdd = new Map<StructureModelClass, StructureModelProperty[]>();
   for (const classData of classes) {
-    classData.properties = findTopologicalSortOfExtends(classData)
-      .flatMap(cls => cls.properties);
+    propertiesToAdd.set(classData, findTopologicalSortOfExtends(classData)
+        .flatMap(cls => cls.properties));
+  }
+  for (const [cls, properties] of propertiesToAdd) {
+    cls.properties = properties;
+  }
+  // Remove all extends because we flattened the hierarchy.
+  for (const cls of classes) {
+    cls.extends = [];
   }
   return result;
 }
