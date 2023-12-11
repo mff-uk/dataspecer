382953caeef95d716f60f2d8dc716eaa7a751d65
Adam Polický <apolicky@profinit.eu>
2023-12-11 21:37:51 +0100

    solve backend inconsistencies

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -1,26 +1,24 @@
-import { getDefaultConfiguration } from "./routes/configuration";
+import {getDefaultConfiguration} from "./routes/configuration";
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 import {
-  addSpecification,
-  cloneSpecification,
-  deleteSpecification,
-  garbageCollection,
-  importSpecifications,
-  listSpecifications,
-  modifySpecification,
+    addSpecification,
+    cloneSpecification,
+    deleteSpecification, garbageCollection, importSpecifications,
+    listSpecifications,
+    modifySpecification
 } from "./routes/specification";
-import { readStore, writeStore } from "./routes/store";
-import { LocalStoreModel } from "./models/local-store-model";
-import { createDataPsm, deleteDataPsm } from "./routes/dataPsm";
+import {readStore, writeStore} from "./routes/store";
+import {LocalStoreModel} from "./models/local-store-model";
+import {createDataPsm, deleteDataPsm} from "./routes/dataPsm";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { generateBikeshedRoute } from "./routes/bikeshed";
-import { DataSpecificationModel } from "./models/data-specification-model";
-import { DataSpecificationWithStores } from "@dataspecer/backend-utils/interfaces";
-import { convertLocalStoresToHttpStores } from "./utils/local-store-to-http-store";
+import {DataSpecificationModel} from "./models/data-specification-model";
+import {DataSpecificationWithStores} from "@dataspecer/backend-utils/interfaces";
+import {convertLocalStoresToHttpStores} from "./utils/local-store-to-http-store";
 import configuration from "./configuration";
-import {HttpStoreDescriptor,  StoreDescriptor } from "@dataspecer/backend-utils/store-descriptor";
+import {HttpStoreDescriptor, StoreDescriptor} from "@dataspecer/backend-utils/store-descriptor";
 import {PackageModel} from "./models/package-model";
 import {
     createPackage, createSemanticModel,
@@ -31,8 +29,6 @@ import {
     updatePackage
 } from "./routes/packages";
 import {LocalStoreDescriptor} from "./models/local-store-descriptor";
-import { asyncHandler } from "./utils/async-handler";
-import { getPackage, writePackage } from "./routes/package";
 
 // Create models
 
@@ -41,11 +37,11 @@ export const prismaClient = new PrismaClient();
 export const dataSpecificationModel = new DataSpecificationModel(storeModel, prismaClient,"https://ofn.gov.cz/data-specification/{}");
 export const packageModel = new PackageModel(storeModel, prismaClient);
 
-export const storeApiUrl = configuration.host + "/store/{}";
+export const storeApiUrl = configuration.host + '/store/{}';
 
 let basename = new URL(configuration.host).pathname;
-if (basename.endsWith("/")) {
-  basename = basename.slice(0, -1);
+if (basename.endsWith('/')) {
+    basename = basename.slice(0, -1);
 }
 
 export function convertStores(stores: LocalStoreDescriptor[]): StoreDescriptor[] {
@@ -64,32 +60,19 @@ export function replaceStoreDescriptorsInDataSpecification<T extends DataSpecifi
 
 const application = express();
 application.use(cors());
-application.use(bodyParser.json({ limit: configuration.payloadSizeLimit }));
-application.use(
-  bodyParser.urlencoded({
-    extended: false,
-    limit: configuration.payloadSizeLimit,
-  })
-);
-application.use(
-  bodyParser.urlencoded({
-    extended: true,
-    limit: configuration.payloadSizeLimit,
-  })
-);
+application.use(bodyParser.json({limit: configuration.payloadSizeLimit}));
+application.use(bodyParser.urlencoded({ extended: false, limit: configuration.payloadSizeLimit }));
+application.use(bodyParser.urlencoded({ extended: true, limit: configuration.payloadSizeLimit }));
 
-application.get(basename + "/data-specification", listSpecifications);
-application.post(basename + "/data-specification", addSpecification);
-application.post(basename + "/data-specification/clone", cloneSpecification);
-application.delete(basename + "/data-specification", deleteSpecification);
-application.put(basename + "/data-specification", modifySpecification);
-application.post(
-  basename + "/data-specification/garbage-collection",
-  garbageCollection
-);
+application.get(basename + '/data-specification', listSpecifications);
+application.post(basename + '/data-specification', addSpecification);
+application.post(basename + '/data-specification/clone', cloneSpecification);
+application.delete(basename + '/data-specification', deleteSpecification);
+application.put(basename + '/data-specification', modifySpecification);
+application.post(basename + '/data-specification/garbage-collection', garbageCollection);
 
-application.post(basename + "/data-specification/data-psm", createDataPsm);
-application.delete(basename + "/data-specification/data-psm", deleteDataPsm);
+application.post(basename + '/data-specification/data-psm', createDataPsm);
+application.delete(basename + '/data-specification/data-psm', deleteDataPsm);
 
 application.post(basename + '/import', importSpecifications);
 
@@ -104,27 +87,18 @@ application.post(basename + '/packages/semantic-models', createSemanticModel);
 
 // Configuration
 
-application.get(basename + "/default-configuration", getDefaultConfiguration);
+application.get(basename + '/default-configuration', getDefaultConfiguration);
 
 // API for reading and writing store content.
 
-application.get(basename + "/store/:storeId", readStore);
-application.put(basename + "/store/:storeId", writeStore);
+application.get(basename + '/store/:storeId', readStore);
+application.put(basename + '/store/:storeId', writeStore);
 
 // API for generators
 
-application.post(
-  basename + "/transformer/bikeshed",
-  bodyParser.text({ type: "*/*", limit: configuration.payloadSizeLimit }),
-  generateBikeshedRoute
-);
-
-application.get(basename + "/packages", getPackage);
-application.post(basename + "/packages", writePackage);
+application.post(basename + '/transformer/bikeshed', bodyParser.text({type:"*/*", limit: configuration.payloadSizeLimit}), generateBikeshedRoute);
 
 application.listen(Number(configuration.port), () => {
-  console.log(`Server is listening on port ${Number(configuration.port)}.`);
-  console.log(
-    `Try ${configuration.host}/data-specification for a list of data specifications. (should return "[]" for new instances) Hmm?`
-  );
+    console.log(`Server is listening on port ${Number(configuration.port)}.`);
+    console.log(`Try ${configuration.host}/data-specification for a list of data specifications. (should return "[]" for new instances)`);
 });
