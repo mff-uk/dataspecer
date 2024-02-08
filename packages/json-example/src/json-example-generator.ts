f24a7c399823e0d6764795702eb9f2d88b53c325
Tereza Miklóšová <t.miklosova@seznam.cz>
2024-02-08 21:40:43 +0100

    Json example json-ld format choice added

--- packages/json-example/src/json-example-generator.ts
+++ packages/json-example/src/json-example-generator.ts
@@ -8,8 +8,11 @@ import {ArtefactGenerator, ArtefactGeneratorContext} from "@dataspecer/core/gene
 import {assertFailed, assertNot} from "@dataspecer/core/core";
 import {transformStructureModel, structureModelAddDefaultValues} from "@dataspecer/core/structure-model/transformation";
 import {JsonExampleAdapter} from "./json-example-adapter.js";
+import {JsonLdAdapter} from "../../json/src/json-ld/json-ld-adapter.js";
+import {DefaultJsonConfiguration, JsonConfiguration, JsonConfigurator} from "../../json/src/configuration";
 import {JSON_SCHEMA, JsonSchemaGenerator} from "@dataspecer/json/json-schema";
 import {MemoryStreamDictionary} from "@dataspecer/core/io/stream/memory-stream-dictionary.js";
+import {DataSpecificationConfigurator, DefaultDataSpecificationConfiguration, DataSpecificationConfiguration} from "@dataspecer/core/data-specification/configuration";
 
 
 interface JsonExampleGeneratorObject {
@@ -32,6 +35,45 @@ export class JsonExampleGenerator implements ArtefactGenerator {
         artefact: DataSpecificationArtefact,
         specification: DataSpecification
       ): Promise<JsonExampleGeneratorObject | null> {
+        var jsonld;
+        const schemaArtefact = artefact as DataSpecificationSchema;
+        const configuration = JsonConfigurator.merge(
+            DefaultJsonConfiguration,
+            JsonConfigurator.getFromObject(schemaArtefact.configuration)
+        ) as JsonConfiguration;
+
+        if(configuration.includeContextInExample){
+            if (!DataSpecificationSchema.is(artefact)) {
+                assertFailed("Invalid artefact type.");
+            }
+
+            const conceptualModel = context.conceptualModels[specification.pim];
+            assertNot(
+                conceptualModel === undefined,
+                `Missing conceptual model ${specification.pim}.`
+            );
+            let model = context.structureModels[schemaArtefact.psm];
+            assertNot(
+                model === undefined,
+                `Missing structure model ${schemaArtefact.psm}.`
+            );
+
+            // Global options for the data specification
+            const globalConfiguration = DataSpecificationConfigurator.merge(
+                DefaultDataSpecificationConfiguration,
+                DataSpecificationConfigurator.getFromObject(schemaArtefact.configuration)
+            ) as DataSpecificationConfiguration;
+        
+            const mergedConceptualModel = {...conceptualModel};
+            mergedConceptualModel.classes = Object.fromEntries(Object.values(context.conceptualModels).map(cm => Object.entries(cm.classes)).flat());
+            model = transformStructureModel(mergedConceptualModel, model, Object.values(context.specifications));
+            model = structureModelAddDefaultValues(model, globalConfiguration);
+        
+            const jsonldadapter = new JsonLdAdapter(model, context, artefact);
+            jsonld = jsonldadapter.generate();
+        }
+
+
         const jsonGenerator = new JsonSchemaGenerator();
 
         const jsonSchema = new DataSpecificationSchema();
@@ -46,8 +88,14 @@ export class JsonExampleGenerator implements ArtefactGenerator {
         const schema = await streamDictionary.readPath(artoutpath).read();
         const s = (schema == null) ? "schema" : schema;
         const adapter = new JsonExampleAdapter(s, context, artefact);
-        return adapter.generate();
-        }
+        if(configuration.includeContextInExample){
+            const data = adapter.generate();
+            const jsonldFakeData = jsonld.slice(0, -1) + "," + (await data).data.substring(1);
+            return { data: jsonldFakeData };
+        } else {
+            return adapter.generate();
+        }    
+    }
 
     async generateToStream(
         context: ArtefactGeneratorContext,
