dc890abe4406254a360a550ead1332f1360eeb55
Štěpán Stenchlák <s.stenchlak@gmail.com>
2024-02-15 13:38:07 +0100

    Fix update pim process

--- applications/client/src/manager/components/update-pim.tsx
+++ applications/client/src/manager/components/update-pim.tsx
@@ -10,6 +10,8 @@ import { useSnackbar } from "notistack";
 import React, { memo, useContext } from "react";
 import { getAdapter } from "../../editor/configuration/adapters/get-adapter";
 import { DataSpecificationsContext } from "../app";
+import { PimAttribute } from "@dataspecer/core/pim/model/pim-attribute";
+import { PimAssociation } from "@dataspecer/core/pim/model/pim-association";
 
 export const UpdatePim = memo(({dataSpecificationIri} : {dataSpecificationIri: string}) => {
     const {dataSpecifications} = useContext(DataSpecificationsContext);
@@ -22,17 +24,21 @@ export const UpdatePim = memo(({dataSpecificationIri} : {dataSpecificationIri: s
         const storeDescriptor = specification.pimStores[0];
         const store = HttpSynchronizedStore.createFromDescriptor(storeDescriptor, httpFetch);
         await store.load();
-        //const allResources = await Promise.all((await store.listResources()).map(iri => store.readResource(iri))) as PimResource[];
+        const allResources = await Promise.all((await store.listResources()).map(iri => store.readResource(iri))) as PimResource[];
 
         const {cimAdapter} = getAdapter(specification.cimAdapters);
 
         const diffPairs: [PimResource, PimResource][] = [];
 
         const classes = await store.listResourcesOfType(PimClass.TYPE);
+        // There should be one PIM class for every CIM class
         for (const pimClassIri of classes) {
             const pimClass = await store.readResource(pimClassIri) as PimClass;
+            if (!pimClass.pimInterpretation) {
+                continue;
+            }
+            
             const cimClass = await cimAdapter.getClass(pimClass.pimInterpretation);
-
             if (!cimClass) {
                 continue;
             }
@@ -40,35 +46,41 @@ export const UpdatePim = memo(({dataSpecificationIri} : {dataSpecificationIri: s
             if (
                 !isEqual(pimClass.pimHumanLabel, cimClass.pimHumanLabel) ||
                 !isEqual(pimClass.pimHumanDescription, cimClass.pimHumanDescription)
-             ) {
+            ) {
                 diffPairs.push([pimClass, cimClass]);
             }
 
-            // const surroundings = await cimAdapter.getSurroundings(pimClass.pimInterpretation);
-            // const surroundingResources = await surroundings.listResources();
-            // for (const surroundingIri of surroundingResources) {
-            //     const surroudingResource = await store.readResource(surroundingIri) as PimResource;
-            //     const pimResources = allResources.filter(r => r.pimInterpretation === surroundingIri);
+            const surroundings = await cimAdapter.getSurroundings(cimClass.pimInterpretation);
+            const surroundingResources = [
+                ...await surroundings.listResourcesOfType(PimAttribute.TYPE),
+                ...await surroundings.listResourcesOfType(PimAssociation.TYPE),
+            ];
+            for (const surroundingIri of surroundingResources) {
+                const surroudingResource = await surroundings.readResource(surroundingIri) as PimResource;
+                const pimResources = allResources.filter(r => r.pimInterpretation === surroudingResource.pimInterpretation);
 
-            //     for (const pimResource of pimResources) {
-            //         if (
-            //             !isEqual(pimResource.pimHumanLabel, surroudingResource.pimHumanLabel) ||
-            //             !isEqual(pimResource.pimHumanDescription, surroudingResource.pimHumanDescription)
-            //         ) {
-            //             diffPairs.push([pimResource, surroudingResource]);
-            //         }
-            //     }
-            // }
+                for (const pimResource of pimResources) {
+                    if (
+                        !isEqual(pimResource.pimHumanLabel, surroudingResource.pimHumanLabel) ||
+                        !isEqual(pimResource.pimHumanDescription, surroudingResource.pimHumanDescription)
+                    ) {
+                        diffPairs.push([pimResource, surroudingResource]);
+                    }
+                }
+            }
+            
         }
 
         for (const [pimResource, cimResource] of diffPairs) {
             if (!isEqual(pimResource.pimHumanLabel, cimResource.pimHumanLabel)) {
+                console.log("Updating human label for ", pimResource, " from label ", pimResource.pimHumanLabel, " to ", cimResource.pimHumanLabel);
                 const op = new PimSetHumanLabel();
                 op.pimResource = pimResource.iri;
                 op.pimHumanLabel = cimResource.pimHumanLabel;
                 await store.applyOperation(op);
             }
             if (!isEqual(pimResource.pimHumanDescription, cimResource.pimHumanDescription)) {
+                console.log("Updating human description for ", pimResource, " from description ", pimResource.pimHumanDescription, " to ", cimResource.pimHumanDescription); 
                 const op = new PimSetHumanDescription();
                 op.pimResource = pimResource.iri;
                 op.pimHumanDescription = cimResource.pimHumanDescription;
@@ -79,7 +91,7 @@ export const UpdatePim = memo(({dataSpecificationIri} : {dataSpecificationIri: s
         await store.save();
         setLoading(false);
 
-        enqueueSnackbar(`${diffPairs.length} PIM classes updated.`, {variant: "success"});
+        enqueueSnackbar(`${diffPairs.length} PIM entities updated.`, {variant: "success"});
     }
 
     return <LoadingButton
