266a24b7ccd5c1c5f8f8e165079c89aa0e0368f6
Štěpán Stenchlák <s.stenchlak@gmail.com>
2024-03-06 04:12:06 +0100

    Fix typo
    Resolves #428: typo in generated documentation (stukturu)

--- packages/json/src/json-schema/json-schema-generator.ts
+++ packages/json/src/json-schema/json-schema-generator.ts
@@ -191,7 +191,7 @@ export class JsonSchemaGenerator implements ArtefactGenerator {
             if (documentationArtefact.templateType === "respec") {
               return render(`<section>
 <h3>Přehled JSON struktury</h3>
-<p>JSON Schéma zachycující stukturu pro <i>{{#humanLabel}}{{translate}}{{/humanLabel}}</i> je definováno v souboru <a href="{{{artifact.json-schema.relativePath}}}"><code>{{artifact.json-schema.relativePath}}</code></a>. ${infoText} <i>{{#structureModel}}{{#roots}}{{#classes}}{{#humanLabel}}{{translate}}{{/humanLabel}}{{/classes}}{{/roots}}{{/structureModel}}</i>.${infoText2}</p>
+<p>JSON Schéma zachycující strukturu pro <i>{{#humanLabel}}{{translate}}{{/humanLabel}}</i> je definováno v souboru <a href="{{{artifact.json-schema.relativePath}}}"><code>{{artifact.json-schema.relativePath}}</code></a>. ${infoText} <i>{{#structureModel}}{{#roots}}{{#classes}}{{#humanLabel}}{{translate}}{{/humanLabel}}{{/classes}}{{/roots}}{{/structureModel}}</i>.${infoText2}</p>
 
 <ul>
 {{#classes}}{{#inThisSchema}}
