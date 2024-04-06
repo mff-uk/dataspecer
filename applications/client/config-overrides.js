2904b5db1a2e548c4e7147a11c85c7283184ba10
Viktor Bujko <viktorbjk@gmail.com>
2024-04-06 18:44:36 +0200

    Fixed noParse regex

--- applications/client/config-overrides.js
+++ applications/client/config-overrides.js
@@ -18,6 +18,6 @@ module.exports = function override(config, env) {
     ];
     // Issue with LdKit generator, specifically with the @ts-morph package
     // https://github.com/microsoft/TypeScript/issues/39436
-    config.module.noParse = /typescript/;
+    config.module.noParse = /typescript\.js/;
     return config;
 };
