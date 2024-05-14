b297b18ba8e098866c6f59e62279cac8ab284254
RadStr <radek.strycek@seznam.cz>
2024-05-14 15:28:46 +0200

    Simple model layouting in manager using ElkJS

---
+++ packages/layout/src/test/graph.ts
@@ -0,0 +1,10 @@
+import { SemanticModelEntity } from "@dataspecer/core-v2/semantic-model/concepts";
+import { GraphClassic } from "../graph-iface";
+import { ExtractedModel } from "../layout-iface";
+
+export function tryCreateClassicGraph(inputModel: Record<string, SemanticModelEntity> | ExtractedModel) {
+    console.log("Calling tryCreateClassicGraph");
+    const graph = new GraphClassic(inputModel);
+    console.log("OUTPUT GRAPH:");
+    console.log(graph);
+}
