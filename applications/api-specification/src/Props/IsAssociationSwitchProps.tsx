33eb1d340bbe8992019c5002c4dd18b089d9d666
Anastasia <anastasia.akhvlediani1998@gmail.com>
2024-05-23 21:07:28 +0100

    OPENAPI: refactoring associationswitch

---
+++ applications/api-specification/src/Props/IsAssociationSwitchProps.tsx
@@ -0,0 +1,28 @@
+import { DataStructure } from '../Models/DataStructureNex';
+import {BaseProps} from './BaseProps';
+
+export interface IsAssociationSwitchProps extends BaseProps 
+{
+    setValue: (path: string, value: any) => void;
+    getValues: any;
+    dataStructureName: string;
+    dataStructures: DataStructure[];
+    setSelectedResponseObject: React.Dispatch<React.SetStateAction<any>>;
+    setResponseObjectFields: React.Dispatch<React.SetStateAction<DataStructure[]>>;
+    setAssociationModeOn: React.Dispatch<React.SetStateAction<boolean>>;
+    defaultValue: string;
+}
+
+// export interface IsAssociationSwitchProps {
+//     index: number;
+//     operationIndex: number;
+//     register: any;
+//     setValue: (path: string, value: any) => void;
+//     getValues: any;
+//     dataStructureName: string;
+//     dataStructures: DataStructure[];
+//     setSelectedResponseObject: React.Dispatch<React.SetStateAction<any>>;
+//     setResponseObjectFields: React.Dispatch<React.SetStateAction<DataStructure[]>>;
+//     setAssociationModeOn: React.Dispatch<React.SetStateAction<boolean>>;
+//     defaultValue: string;
+// }
