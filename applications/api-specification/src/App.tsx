f537f76f480512f8a85f54c181f44df8bb933dec
Anastasia <anastasia.akhvlediani1998@gmail.com>
2024-05-24 23:14:43 +0100

    OPENAPI: commenting unnecessary files

--- applications/api-specification/src/App.tsx
+++ applications/api-specification/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {ApiSpecificationForm} from './UpdatedMainForm.tsx'
-import {SplitScreen} from './SplitScreen.tsx'
 
 function App() {
   return (
