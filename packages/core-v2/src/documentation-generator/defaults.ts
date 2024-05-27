7ea0dd30400cbf8a313e665e210aeb9996c42b64
Štěpán Stenchlák <s.stenchlak@gmail.com>
2024-05-27 08:06:32 +0200

    Documentation generator improvements

--- packages/core-v2/src/documentation-generator/defaults.ts
+++ packages/core-v2/src/documentation-generator/defaults.ts
@@ -2,10 +2,11 @@ import { DocumentationGeneratorConfiguration } from "./generator";
 
 export const defaultConfiguration: DocumentationGeneratorConfiguration = {
   template: `<!DOCTYPE html>
+  {{#def "class"}}<a href="{{href id}}">{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}</a>{{/def}}
   <html>
     <head>
       <meta charset="utf-8" />
-      <title>{{package.userMetadata.label.cs}} ({{package.iri}})</title>
+      <title>{{translate package.userMetadata.label}}</title>
       <script
         src="https://www.w3.org/Tools/respec/respec-w3c"
         class="remove"
@@ -15,41 +16,135 @@ export const defaultConfiguration: DocumentationGeneratorConfiguration = {
         // All config options at https://respec.org/docs/
         var respecConfig = {
           specStatus: "ED",
-          editors: [{ name: "Dataspecer", url: "https://dataspecer.com" }],
+          editors: [{ name: "Dataspecer", url: "https://dataspecer.com" }]
           //github: "some-org/mySpec",
           //shortName: "dahut",
           //xref: "web-platform",
           //group: "my-working-group",
         };
       </script>
+          <style>
+      .figure img, .sidefigure img, figure img, .figure object, .sidefigure object, figure object, img, .img {
+      max-width: 100%;
+      margin: auto;
+      height: auto;
+      }
+    </style>
     </head>
     <body>
       <section id="abstract">
-        <p>This file documents package <strong>{{package.userMetadata.label.cs}}</strong>.</p>
+        <p>This file documents package {{#translate package.userMetadata.label}}<strong>{{translation}}</strong>{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}.</p>
       </section>
       <section id="sotd">
         <p>This is required.</p>
       </section>
 
+      {{#each semanticModels}}
+        <section>
+          <h1>Vocabulary Overview</h1>
+          <h2>Classes</h2>
+          This section lists the classes matching the base namespace of this vocabulary.
 
-      <section>
-        <h2>Semantic models</h2>
-        <p>This section describes all semantic models that are contained within the package.</p>
+          {{#each this}}
+        
+            {{#ifEquals type.[0] "class"}}
+                <section id="{{anchor}}">
+                <h4>{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}</h4>
 
-        {{#each semanticModels}}
-          <section>
-            <h3>Semantic model</h3>
-            <h4>Classes</h4>
-            <ul>
-              {{#each this}}
-                {{#ifEquals type.[0] "class"}}
-                  <li>{{iri}}: {{name.cs}} {{name.en}}</li>
-                {{/ifEquals}}
-              {{/each}}
-            </ul>
-          </section>
-        {{/each}}
-      </section>
+                <table class="def">
+                  <tr>
+                    <td>Internal ID</td>
+                    <td><a href="{{href id}}">{{id}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Public IRI</td>
+                    <td><a href="{{iri}}">{{iri}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Description</td>
+                    <td>{{translate description}}</td>
+                  </tr>
+                  <tr>
+                    <td>Parent classes</td>
+                    <td>{{#each (parentClasses id)}}{{class}}{{#unless @last}}, {{/unless}}{{/each}}</td>
+                  </tr>
+                  <tr>
+                    <td>Sub-classes</td>
+                    <td>{{#each (subClasses id)}}{{class}}{{#unless @last}}, {{/unless}}{{/each}}</td>
+                  </tr>
+                </table>
+              </section>
+            {{/ifEquals}}    
+          {{/each}}
+  
+
+          <h2>Profiles of classes</h2>
+          This section lists the profiles of classes.
+
+
+          {{#each this}}
+            {{#ifEquals type.[0] "class-usage"}}
+              <section id="{{anchor}}">
+                <h4>{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}</h4>
+
+                <table class="def">
+                  <tr>
+                    <td>Internal ID</td>
+                    <td><a href="{{href id}}">{{id}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Public IRI</td>
+                    <td><a href="{{iri}}">{{iri}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Description</td>
+                    <td>{{translate description}}</td>
+                  </tr>
+                  <tr>
+                    <td>Usage Note</td>
+                    <td>{{translate usageNote}}</td>
+                  </tr>
+                </table>
+              </section>
+            {{/ifEquals}}
+          {{/each}}
+
+          <h2>Properties</h2>
+          {{#each this}}
+            {{#ifEquals type.[0] "relationship"}}
+              <section id="{{anchor}}">
+
+                <h4>{{#translate ends.1.name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}</h4>
+
+                <table class="def">
+                  <tr>
+                    <td>Internal ID</td>
+                    <td><a href="{{href id}}">{{id}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Public IRI</td>
+                    <td><a href="{{ends.1.iri}}">{{ends.1.iri}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Description</td>
+                    <td>{{translate ends.1.description}}</td>
+                  </tr>
+                  <tr>
+                    <td>Domain</td>
+                    <td><a href="{{href ends.0.concept}}">{{#semanticEntity ends.0.concept}}{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}{{/semanticEntity}}</a></td>
+                  </tr>
+                  <tr>
+                    <td>Range</td>
+                    <td><a href="{{href ends.1.concept}}">{{#semanticEntity ends.1.concept}}{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>without assigned name</i>{{/translate}}{{/semanticEntity}}</a></td>
+                  </tr>
+                </table>
+              </section>
+            {{/ifEquals}}
+          {{/each}}
+
+
+        </section>
+      {{/each}}
 
     </body>
   </html>`,
