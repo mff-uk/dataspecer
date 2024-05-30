5db190d67a037ffbfb13bcd43838775b36768c8c
apolicky <adam.policky@gmail.com>
2024-05-30 16:21:29 +0200

    dscme - make the page a little nicer

--- applications/conceptual-model-editor/src/app/page.tsx
+++ applications/conceptual-model-editor/src/app/page.tsx
@@ -15,22 +15,22 @@ export default function Page() {
             <div className="mx-auto max-w-screen-lg">
                 <h1 className="mb-12 mt-2 flex flex-row px-6 text-3xl font-bold tracking-tight text-gray-900">
                     Home of
-                    <span className="mx-2 rounded-md bg-[#5438dc] px-2">
+                    <span className="mx-2 rounded-full bg-[#5438dc] px-2">
                         <DscmeLogo />
                     </span>
                 </h1>
-                <ul className="[&>li]:mb-4">
+                <ul className="px-2 [&>li]:mb-6 [&>li]:flex [&>li]:w-full [&>li]:flex-col md:[&>li]:flex-row">
                     <li>
                         <p>
-                            You can either start playing with the editor right away. Hover you won't be able to save
+                            You can either start playing with the editor right away. However you won't be able to save
                             your work to our backend. There will still be the option to save your workspace to{" "}
                             <span className="font-mono text-[#5438dc]">.json</span> or export{" "}
                             <span className="font-mono text-[#5438dc]">lightweight ontology.</span>
                         </p>
-                        <p className="mt-2">
+                        <p className="mx-auto mt-3 md:my-auto md:ml-2 md:mr-0 md:flex-grow">
                             <Link
                                 href={"/core-v2"}
-                                className="border border-black px-2 py-1 hover:bg-[#5438dc] hover:text-white"
+                                className="text-nowrap border border-black px-2 py-1 hover:bg-[#5438dc] hover:text-white"
                             >
                                 work w/o packages
                             </Link>
@@ -41,9 +41,9 @@ export default function Page() {
                             To be able to save your work to our backend and have it accessible from anywhere, open up
                             the <span className="font-mono text-[#5438dc]"></span>
                         </p>
-                        <p className="mt-2">
+                        <p className="mx-auto mt-3 text-right md:my-auto md:ml-2 md:mr-0 md:flex-grow">
                             <a
-                                className="border border-black px-2 py-1 hover:bg-[#5438dc] hover:text-white"
+                                className="text-nowrap border border-black px-2 py-1 hover:bg-[#5438dc] hover:text-white "
                                 href={managerPath}
                             >
                                 package manager
