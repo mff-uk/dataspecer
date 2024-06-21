44f1ecf78837d97c08542236148b8b4ab9bfdf84
Štěpán Stenchlák <s.stenchlak@gmail.com>
2024-06-21 08:34:47 +0200

    Documentation: update default template

--- packages/core-v2/src/documentation-generator/defaults.ts
+++ packages/core-v2/src/documentation-generator/defaults.ts
@@ -42,110 +42,133 @@ export const defaultConfiguration: DocumentationGeneratorConfiguration = {
         <p>todo.</p>
       </section>
 
-      {{#each semanticModels}}
-        <section>
-          <h1>{{#iflng "cs"}}Slovník{{lng}}Vocabulary Overview{{/iflng}}</h1>
-          <h2>{{#iflng "cs"}}Třídy{{lng}}Classes{{/iflng}}</h2>
-          {{#iflng "cs"}}Tato sekce popisuje všechny třídy v tomto slovníku.
-          {{lng}}This section lists the classes matching the base namespace of this vocabulary.
-          {{/iflng}}
-          
+      <section>
+        <h1>{{#iflng "cs"}}Přílohy{{lng}}Attachements{{/iflng}}</h1>
+        <table class="def">
+          {{#if externalArtifacts.owl-vocabulary}}
+            <tr>
+              <td>OWL</td>
+              <td><a href="{{externalArtifacts.owl-vocabulary.[0].URL}}">{{externalArtifacts.owl-vocabulary.[0].URL}}</a></td>
+            </tr>
+          {{/if}}
+          {{#if externalArtifacts.dsv-profile}}
+            <tr>
+              <td>{{#iflng "cs"}}DSV profil{{lng}}DSV profile{{/iflng}}</td>
+              <td><a href="{{externalArtifacts.dsv-profile.[0].URL}}">{{externalArtifacts.dsv-profile.[0].URL}}</a></td>
+            </tr>
+          {{/if}}
+        </table>
+      </section>
 
-          {{#each this}}
+      <section>
+        <h1>{{#iflng "cs"}}Slovník{{lng}}Vocabulary Overview{{/iflng}}</h1>
+
+        {{#each externalArtifacts.svg}}
+          <figure>
+            <img src="{{URL}}" alt="alt text 2" />
+            <figcaption>title</figcaption>
+          </figure>
+        {{/each}}
+
+        <h2>{{#iflng "cs"}}Třídy{{lng}}Classes{{/iflng}}</h2>
+        {{#iflng "cs"}}Tato sekce popisuje všechny třídy v tomto slovníku.
+        {{lng}}This section lists the classes matching the base namespace of this vocabulary.
+        {{/iflng}}
         
-            {{#ifEquals type.[0] "class"}}
-                <section id="{{anchor}}">
-                <h4>{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}</h4>
 
-                <table class="def">
-                  <tr>
-                    <td>IRI</td>
-                    <td><a href="{{iri}}">{{iri}}</a></td>
-                  </tr>
-                  {{#translate description}}
-                  <tr>
-                    <td>{{#iflng "cs"}}Popis{{lng}}Description{{/iflng}}</td>
-                    <td>{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}</td>
-                  </tr>
-                  {{/translate}}
-                  {{#if (parentClasses id)}}
-                  <tr>
-                    <td>{{#iflng "cs"}}Rodičovské třídy{{lng}}Parent classes{{/iflng}}</td>
-                    <td>{{#each (parentClasses id)}}{{class}}{{#unless @last}}, {{/unless}}{{/each}}</td>
-                  </tr>
-                  {{/if}}
-                  {{#if (subClasses id)}}
-                  <tr>
-                    <td>{{#iflng "cs"}}Podtřídy z tohoto slovníku{{lng}}Sub-classes{{/iflng}}</td>
-                    <td>{{#each (subClasses id)}}{{class}}{{#unless @last}}, {{/unless}}{{/each}}</td>
-                  </tr>
-                  {{/if}}
-                </table>
-              </section>
-            {{/ifEquals}}    
-          {{/each}}
-  
-          {{#each this}}
-            {{#ifEquals type.[0] "class-usage"}}
+        {{#each locallyDefinedSemanticEntity}}
+          {{#ifEquals type.[0] "class"}}
               <section id="{{anchor}}">
-                <h4>{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}</h4>
+              <h4>{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}</h4>
 
-                <table class="def">
-                  <tr>
-                    <td>Internal ID</td>
-                    <td><a href="{{href id}}">{{id}}</a></td>
-                  </tr>
-                  <tr>
-                    <td>Public IRI</td>
-                    <td><a href="{{iri}}">{{iri}}</a></td>
-                  </tr>
-                  <tr>
-                    <td>Description</td>
-                    <td>{{translate description}}</td>
-                  </tr>
-                  <tr>
-                    <td>Usage Note</td>
-                    <td>{{translate usageNote}}</td>
-                  </tr>
-                </table>
-              </section>
-            {{/ifEquals}}
-          {{/each}}
+              <table class="def">
+                <tr>
+                  <td>IRI</td>
+                  <td><a href="{{iri}}">{{iri}}</a></td>
+                </tr>
+                {{#translate description}}
+                <tr>
+                  <td>{{#iflng "cs"}}Popis{{lng}}Description{{/iflng}}</td>
+                  <td>{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}</td>
+                </tr>
+                {{/translate}}
+                {{#if (parentClasses id)}}
+                <tr>
+                  <td>{{#iflng "cs"}}Rodičovské třídy{{lng}}Parent classes{{/iflng}}</td>
+                  <td>{{#each (parentClasses id)}}{{class}}{{#unless @last}}, {{/unless}}{{/each}}</td>
+                </tr>
+                {{/if}}
+                {{#if (subClasses id)}}
+                <tr>
+                  <td>{{#iflng "cs"}}Podtřídy z tohoto slovníku{{lng}}Sub-classes{{/iflng}}</td>
+                  <td>{{#each (subClasses id)}}{{class}}{{#unless @last}}, {{/unless}}{{/each}}</td>
+                </tr>
+                {{/if}}
+              </table>
+            </section>
+          {{/ifEquals}}    
+        {{/each}}
 
-          <h2>{{#iflng "cs"}}Vlastnosti (atributy a asociace){{lng}}Properties (attributes and associations){{/iflng}}</h2>
-          {{#each this}}
-            {{#ifEquals type.[0] "relationship"}}
-              <section id="{{anchor}}">
+        {{#each locallyDefinedSemanticEntity}}
+          {{#ifEquals type.[0] "class-usage"}}
+            <section id="{{anchor}}">
+              <h4>{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}</h4>
 
-                <h4>{{#translate ends.1.name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}</h4>
+              <table class="def">
+                <tr>
+                  <td>Internal ID</td>
+                  <td><a href="{{href id}}">{{id}}</a></td>
+                </tr>
+                <tr>
+                  <td>Public IRI</td>
+                  <td><a href="{{iri}}">{{iri}}</a></td>
+                </tr>
+                <tr>
+                  <td>Description</td>
+                  <td>{{translate description}}</td>
+                </tr>
+                <tr>
+                  <td>Usage Note</td>
+                  <td>{{translate usageNote}}</td>
+                </tr>
+              </table>
+            </section>
+          {{/ifEquals}}
+        {{/each}}
 
-                <table class="def">
-                  <tr>
-                    <td>IRI</td>
-                    <td><a href="{{ends.1.iri}}">{{ends.1.iri}}</a></td>
-                  </tr>
-                  {{#translate ends.1.description}}
-                  <tr>
-                    <td>{{#iflng "cs"}}Popis{{lng}}Description{{/iflng}}</td>
-                    <td>{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}</td>
-                  </tr>
-                  {{/translate}}
-                  <tr>
-                    <td>{{#iflng "cs"}}Doména{{lng}}Domain{{/iflng}}</td>
-                    <td><a href="{{href ends.0.concept}}">{{#semanticEntity ends.0.concept}}{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}{{/semanticEntity}}</a></td>
-                  </tr>
-                  <tr>
-                    <td>{{#iflng "cs"}}Rozsah{{lng}}Range{{/iflng}}</td>
-                    <td><a href="{{href ends.1.concept}}">{{#semanticEntity ends.1.concept}}{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}{{/semanticEntity}}</a></td>
-                  </tr>
-                </table>
-              </section>
-            {{/ifEquals}}
-          {{/each}}
+        <h2>{{#iflng "cs"}}Vlastnosti (atributy a asociace){{lng}}Properties (attributes and associations){{/iflng}}</h2>
+        {{#each locallyDefinedSemanticEntity}}
+          {{#ifEquals type.[0] "relationship"}}
+            <section id="{{anchor}}">
+
+              <h4>{{#translate ends.1.name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}</h4>
+
+              <table class="def">
+                <tr>
+                  <td>IRI</td>
+                  <td><a href="{{ends.1.iri}}">{{ends.1.iri}}</a></td>
+                </tr>
+                {{#translate ends.1.description}}
+                <tr>
+                  <td>{{#iflng "cs"}}Popis{{lng}}Description{{/iflng}}</td>
+                  <td>{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}</td>
+                </tr>
+                {{/translate}}
+                <tr>
+                  <td>{{#iflng "cs"}}Doména{{lng}}Domain{{/iflng}}</td>
+                  <td><a href="{{href ends.0.concept}}">{{#semanticEntity ends.0.concept}}{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}{{/semanticEntity}}</a></td>
+                </tr>
+                <tr>
+                  <td>{{#iflng "cs"}}Rozsah{{lng}}Range{{/iflng}}</td>
+                  <td><a href="{{href ends.1.concept}}">{{#semanticEntity ends.1.concept}}{{#translate name}}{{translation}}{{#if otherLang}} (@{{otherLang}}){{/if}}{{else}}<i>{{#iflng "cs"}}beze jména{{lng}}without assigned name{{/iflng}}</i>{{/translate}}{{/semanticEntity}}</a></td>
+                </tr>
+              </table>
+            </section>
+          {{/ifEquals}}
+        {{/each}}
 
 
-        </section>
-      {{/each}}
+      </section>
 
     </body>
   </html>`,
