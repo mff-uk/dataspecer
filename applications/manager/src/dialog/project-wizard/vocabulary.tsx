5bc25f651d8bd5e0a1108fcf62cdd95757dd3157
Štěpán Stenchlák <s.stenchlak@gmail.com>
2024-07-01 14:46:43 +0200

    Improve UI loading

--- applications/manager/src/dialog/project-wizard/vocabulary.tsx
+++ applications/manager/src/dialog/project-wizard/vocabulary.tsx
@@ -1,53 +1,61 @@
 import { Modal, ModalBody, ModalContent, ModalHeader, ModalTitle } from "@/components/modal";
-import { Button } from "@/components/ui/button";
+import { LoadingButton } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { createModelInstructions, getCMELink } from "@/known-models";
 import { BetterModalProps } from "@/lib/better-modal";
 import { LOCAL_PACKAGE, LOCAL_SEMANTIC_MODEL, LOCAL_VISUAL_MODEL } from "@dataspecer/core-v2/model/known-models";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 
 export const Vocabulary = ({ isOpen, resolve, iri }: { iri: string } & BetterModalProps<boolean>) => {
   const {t, i18n} = useTranslation();
+  const [loading, setLoading] = useState(false);
 
   const formSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     event.stopPropagation();
+    setLoading(true);
 
-    const name = (event.target as any)["name"].value;
-
-    // Create package
-    const packageIri = await createModelInstructions[LOCAL_PACKAGE].createHook({
-      parentIri: iri,
-      label: {[i18n.language]: name},
-      description: {[i18n.language]: (event.target as any)["description"].value},
-      documentBaseUrl: (event.target as any)["documentation-url"].value ?? undefined,
-    }) as string;
-
-    // Create semantic model
-    await createModelInstructions[LOCAL_SEMANTIC_MODEL].createHook({
-      parentIri: packageIri,
-      label: {en: "Semantic model"},
-      description: {en: "Semantic model for the vocabulary"},
-      baseIri: (event.target as any)["base-url"].value,
-      documentBaseUrl: (event.target as any)["documentation-url"].value ?? undefined,
-      modelAlias: name,
-    });
-
-    // Create view model
-    const viewIri = await createModelInstructions[LOCAL_VISUAL_MODEL].createHook({
-      parentIri: packageIri,
-      label: {en: "Main view"},
-      description: {en: "View model for the vocabulary"},
-    }) as string;
-
-    // Redirect to url
-    window.location.href = getCMELink(packageIri, viewIri);
-
-    // Never resolve as we need to redirect!
-    // resolve(true);
+    try {
+      const name = (event.target as any)["name"].value;
+  
+      // Create package
+      const packageIri = await createModelInstructions[LOCAL_PACKAGE].createHook({
+        parentIri: iri,
+        label: {[i18n.language]: name},
+        description: {[i18n.language]: (event.target as any)["description"].value},
+        documentBaseUrl: (event.target as any)["documentation-url"].value ?? undefined,
+      }) as string;
+  
+      // Create semantic model
+      await createModelInstructions[LOCAL_SEMANTIC_MODEL].createHook({
+        parentIri: packageIri,
+        label: {en: "Semantic model"},
+        description: {en: "Semantic model for the vocabulary"},
+        baseIri: (event.target as any)["base-url"].value,
+        documentBaseUrl: (event.target as any)["documentation-url"].value ?? undefined,
+        modelAlias: name,
+      });
+  
+      // Create view model
+      const viewIri = await createModelInstructions[LOCAL_VISUAL_MODEL].createHook({
+        parentIri: packageIri,
+        label: {en: "Main view"},
+        description: {en: "View model for the vocabulary"},
+      }) as string;
+  
+      // Redirect to url
+      window.location.href = getCMELink(packageIri, viewIri);
+  
+      // Never resolve as we need to redirect!
+      // resolve(true);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
   }
 
   return (
@@ -78,7 +86,7 @@ export const Vocabulary = ({ isOpen, resolve, iri }: { iri: string } & BetterMod
               <Label htmlFor="authors">{t("form.authors.name")}</Label>
               <Textarea id="authors" placeholder={t("form.authors.instruction")} />
             </div> */}
-            <Button type="submit">{t("form.create-button.name")}</Button>
+            <LoadingButton type="submit" loading={loading}>{t("form.create-button.name")}</LoadingButton>
           </form>
         </ModalBody>
       </ModalContent>
