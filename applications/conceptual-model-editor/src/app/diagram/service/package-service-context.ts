e6d7d38f57acd718c7ca8d605bcad3ed73fd4f71
Petr Škoda <skodapetr@gmail.com>
2024-07-16 11:08:28 +0200

    Add service for working with packages
    Overtime it should replace backend connection, also add a future single
    API for HTTP requests so we can employ caching.

---
+++ applications/conceptual-model-editor/src/app/diagram/service/package-service-context.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+import { type Package } from "@dataspecer/core-v2/project";
+
+import { useQueryParamsContext } from "../context/query-params-context";
+import { packageService } from "./package-service";
+
+export interface UsePackageServiceType {
+  /**
+   * Identifier of the current package.
+   */
+  currentPackageIdentifier: string | null;
+  /**
+   * Current package.
+   */
+  currentPackage: Package | null;
+}
+
+/**
+ * Expose PackageService using a context.
+ */
+export const usePackageService = (): UsePackageServiceType => {
+  const { packageId } = useQueryParamsContext();
+  const [currentPackage, setCurrentPackage] = useState<Package | null>(null);
+
+  useEffect(() => fetchCurrentPackage(packageId, setCurrentPackage), [packageId]);
+
+  return {
+    currentPackageIdentifier: packageId,
+    currentPackage,
+  };
+};
+
+const fetchCurrentPackage = (packageId: string | null, setCurrentPackage: (nextPackage: Package | null) => void): void => {
+  if (packageId === null) {
+    return;
+  }
+  void (async () => {
+    const nextPackage = await packageService.getPackage(packageId);
+    setCurrentPackage(nextPackage);
+  })();
+};
---
+++ applications/conceptual-model-editor/src/app/diagram/service/package-service.ts
@@ -0,0 +1,28 @@
+import { BackendPackageService, type Package } from "@dataspecer/core-v2/project";
+import { fetchService } from "../service/fetch-service";
+
+export interface PackageService {
+  /**
+   * Fetch information about a package.
+   */
+  getPackage: (identifier: string) => Promise<Package>;
+  /**
+   * Return information about the root package.
+   */
+  getRootPackage: (identifier: string) => Promise<Package>;
+}
+
+const backendService = new BackendPackageService(process.env.NEXT_PUBLIC_APP_BACKEND!, fetchService.fetch);
+
+const backendPackageRootUrl = process.env.NEXT_PUBLIC_APP_BACKEND_PACKAGE_ROOT!;
+
+const getPackage = (identifier: string): Promise<Package> => {
+  return backendService.getPackage(identifier);
+};
+
+const getRootPackage = () => getPackage(backendPackageRootUrl);
+
+export const packageService: PackageService = {
+  getPackage,
+  getRootPackage,
+};
