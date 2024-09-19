14d13b08382122335625cfada5dae489beb8de62
Viktor Bujko <viktorbjk@gmail.com>
2024-09-19 22:06:49 +0200

    Temporarily disable types generation within ldkit schema

--- packages/ldkit/src/ldkit-schema-adapter.ts
+++ packages/ldkit/src/ldkit-schema-adapter.ts
@@ -1,6 +1,6 @@
 import { LdkitSchema, LdkitSchemaProperty, LdkitSchemaPropertyMap } from "./ldkit-schema-model";
 import { StructureModelProperty } from "@dataspecer/core/structure-model/model/structure-model-property";
-import { StructureModelComplexType, StructureModelPrimitiveType, StructureModelType } from "@dataspecer/core/structure-model/model/structure-model-type";
+import { StructureModelComplexType, StructureModelPrimitiveType } from "@dataspecer/core/structure-model/model/structure-model-type";
 import { assert } from "@dataspecer/core/core";
 import { StructureModel } from "@dataspecer/core/structure-model/model/structure-model";
 import { StructureModelClass } from "@dataspecer/core/structure-model/model/structure-model-class";
@@ -14,17 +14,17 @@ export class LdkitSchemaAdapter implements StructureClassToSchemaAdapter {
 
     convertAttributeDataTypeToLdkitSupportedDataType<T>(dataTypeIri: string): keyof T {
         const map: { [k: string]: any } = {
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/boolean": xsd.boolean,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/datum": xsd.date,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/datum-a-čas": xsd.dateTime,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/celé-číslo": xsd.integer,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/desetinné-číslo": xsd.decimal,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/url": xsd.anyURI,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/řetězec": xsd.string,
-            "https://ofn.gov.cz/zdroj/z%C3%A1kladn%C3%AD-datov%C3%A9-typy/2020-07-01/text":  rdf.langString
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/boolean": "xsd.boolean",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/datum": "xsd.date",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/datum-a-čas": "xsd.dateTime",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/celé-číslo": "xsd.integer",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/desetinné-číslo": "xsd.decimal",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/url": "xsd.anyURI",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/řetězec": "xsd.string",
+            "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/text":  "rdf.langString"
         }
 
-        return map[dataTypeIri] ?? xsd.string;
+        return map[dataTypeIri] ?? "xsd.string";
     }
 
     convertAttributePropertyToLdkitProperty(attribute: StructureModelPrimitiveType, ldkitProperty: LdkitSchemaProperty): LdkitSchemaProperty {
@@ -39,7 +39,7 @@ export class LdkitSchemaAdapter implements StructureClassToSchemaAdapter {
         if (attribute.dataType === multiLangDatatypeIri) {
             ldkitProperty["@multilang"] = true;
         }
-        ldkitProperty["@type"] = this.convertAttributeDataTypeToLdkitSupportedDataType(attribute.dataType);
+        //ldkitProperty["@type"] = this.convertAttributeDataTypeToLdkitSupportedDataType(attribute.dataType);
         return ldkitProperty;
     }
 
@@ -49,7 +49,7 @@ export class LdkitSchemaAdapter implements StructureClassToSchemaAdapter {
         }
 
         const nestedClass: StructureModelClass = association.dataType;
-        ldkitProperty["@type"] = xsd.string; //this.convertDataTypeToLdkitSupportedDataType(association); //nestedClass.cimIri;
+        //ldkitProperty["@type"] = xsd.string; //this.convertDataTypeToLdkitSupportedDataType(association); //nestedClass.cimIri;
         ldkitProperty["@schema"] = this.convertStructureModelClassToLdkitSchema(nestedClass);
 
         return ldkitProperty;
