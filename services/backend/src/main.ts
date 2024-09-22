73cc4ce63765961815468fe1097f10b7847b0241
Viktor Bujko <viktorbjk@gmail.com>
2024-09-22 21:22:49 +0200

    [WIP]: Backend integration

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -21,6 +21,7 @@ import { getDocumentation, getLightweightOwl, getSingleFile, getZip, getlightwei
 import { generate } from './routes/generate';
 import { importResource } from './routes/import';
 import { migratePR419 } from './tools/migrate-pr419';
+import { getGeneratedApplication } from './routes/genapp';
 
 // Create application models
 
@@ -117,6 +118,10 @@ application.get(basename + '/generate', generate);
 application.get(basename + '/experimental/output.zip', getZip);
 application.get(basename + '/preview/*', getSingleFile);
 
+// Generate application
+
+application.get(basename + "/generate-app", getGeneratedApplication);
+
 (async () => {
     // Command-line arguments
     if (process.argv.length > 2) {
@@ -139,7 +144,7 @@ application.get(basename + '/preview/*', getSingleFile);
             console.log("There is no root package for data specifications from v1 dataspecer. Creating one...");
             await resourceModel.createPackage(null, configuration.v1RootIri, configuration.v1RootMetadata);
         }
-    
+
         application.listen(Number(configuration.port), () => {
             console.log(`Server is listening on port ${Number(configuration.port)}.`);
             console.log(`Try ${configuration.host}/data-specification for a list of data specifications. (should return "[]" for new instances)`);
