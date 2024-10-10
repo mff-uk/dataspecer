ebdc5429490218914ac1389179913687df7feb3e
Viktor Bujko <viktorbjk@gmail.com>
2024-10-10 23:09:23 +0200

    added clear box to page bottom

--- packages/genapp/templates/scaffolding/copy/src/Content.tsx
+++ packages/genapp/templates/scaffolding/copy/src/Content.tsx
@@ -5,6 +5,12 @@ export const Content = ({ children }) => {
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
             <Toolbar />
             { children }
+            <Box component="section" sx={{
+                p: 1,
+                color: "transparent",
+                width: window.innerWidth,
+                height: "10%"
+            }} />
         </Box>
     )
 }
