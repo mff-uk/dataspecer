d4b26b92b53144c3785a06100ad23b6ecd37dc32
Petr Škoda <skodapetr@gmail.com>
2024-11-06 12:42:12 +0100

    Fix edge dragging
    #763

--- applications/conceptual-model-editor/src/diagram/edge/connection-edge.tsx
+++ applications/conceptual-model-editor/src/diagram/edge/connection-edge.tsx
@@ -1,20 +1,25 @@
-import { type ConnectionLineComponent } from "@xyflow/react";
+import type { XYPosition, ConnectionLineComponent } from "@xyflow/react";
 
 import { findRectangleLineIntersection } from "./math";
 
-const SOURCE_BORDER = 6;
+const BORDER = 6;
 
 /**
  * This edge is used when user drag from a port/handle.
  */
 export const ConnectionEdge: ConnectionLineComponent = (props) => {
 
+  const fromNode = props.fromNode;
+  if (insideBox(props.toX, props.toY, fromNode.position, fromNode.measured)) {
+    return null;
+  }
+
   const source = findRectangleLineIntersection({
     x: props.fromX,
     y: props.fromY
   }, {
-    width: (props.fromNode.measured.width ?? 0) + SOURCE_BORDER,
-    height: (props.fromNode.measured.height ?? 0) + SOURCE_BORDER,
+    width: (props.fromNode.measured.width ?? 0) + BORDER,
+    height: (props.fromNode.measured.height ?? 0) + BORDER,
   }, {
     x: props.toX,
     y: props.toY
@@ -40,3 +45,15 @@ export const ConnectionEdge: ConnectionLineComponent = (props) => {
     </g>
   );
 };
+
+function insideBox(
+  x: number, y: number,
+  position: XYPosition, measured: { width?: number; height?: number },
+): boolean {
+  const relativeX = x - position.x;
+  const relativeY = y - position.y;
+  return relativeX >= -BORDER
+    && relativeY >= -BORDER
+    && relativeX <= (measured.width ?? 0) + (2 * BORDER)
+    && relativeY <= (measured.height ?? 0) + (2 * BORDER);
+}
