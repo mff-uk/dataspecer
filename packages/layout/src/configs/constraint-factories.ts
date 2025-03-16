c4859564f3997c093c531574878921b3d8e7b6af
RadStr <radek.strycek@seznam.cz>
2025-03-16 23:30:45 +0100

    Fix incorrect parameter deconstructing in foreach

--- packages/layout/src/configs/constraint-factories.ts
+++ packages/layout/src/configs/constraint-factories.ts
@@ -300,8 +300,8 @@ export const SPECIFIC_ALGORITHM_CONVERSIONS_MAP: Record<SpecificGraphConversions
             // TODO RadStr: Commented code - old variant
             const sectorPopulations = GraphAlgorithms.findSectorNodePopulation(graph, clusterRoot, edgesInCluster, ToConsiderFilter.OnlyNotLayouted);
             const sectorPopulationsEdges = GraphAlgorithms.findSectorEdgePopulation(graph, clusterRoot, edgesInCluster, ToConsiderFilter.OnlyNotLayouted);
-            Object.entries(sectorPopulationsEdges).forEach((sector, population) => {
-                sectorPopulations[sector as unknown as Direction] += population;
+            Object.entries(sectorPopulationsEdges).forEach(([sector, population]) => {
+                sectorPopulations[sector as Direction] += population;
             });
             const populatedSectorsAscending = Object.entries(sectorPopulations)
                 .sort(([, edgesA], [, edgesB]) => edgesA - edgesB);
