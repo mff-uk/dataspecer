6995bdbadc247f8e330d9f3718948611dadfb63c
Petr Škoda <skodapetr@gmail.com>
2025-03-28 10:18:23 +0100

    Update model create functionality
    Introduce semantic model factory, update default new mode URL, #1107.
    
    Signed-off-by: Petr Škoda <skodapetr@gmail.com>

--- applications/conceptual-model-editor/src/action/open-create-vocabulary.ts
+++ applications/conceptual-model-editor/src/action/open-create-vocabulary.ts
@@ -1,7 +1,4 @@
-import { createRdfsModel, createSgovModel } from "@dataspecer/core-v2/semantic-model/simplified";
-import { InMemorySemanticModel } from "@dataspecer/core-v2/semantic-model/in-memory";
 import { EntityModel } from "@dataspecer/core-v2";
-import { httpFetch } from "@dataspecer/core/io/fetch/fetch-browser";
 
 import { createLogger } from "../application";
 import { ModelGraphContextType } from "../context/model-context";
@@ -10,6 +7,7 @@ import { createAddModelDialog } from "../dialog/semantic-model/create-model-dial
 import { CreateModelState, TabType } from "../dialog/semantic-model/create-model-dialog-controller";
 import { randomColorFromPalette } from "../util/color-utils";
 import { createDefaultWritableVisualModel } from "../dataspecer/visual-model/visual-model-factory";
+import { createCzechSemanticVocabulary, createInMemorySemanticModel, createRdfsSemanticModel } from "@/dataspecer/semantic-model/semantic-model-factory";
 
 const LOG = createLogger(import.meta.url);
 
@@ -18,68 +16,49 @@ export function openCreateVocabularyAction(
   graph: ModelGraphContextType,
 ) {
   const onConfirm = (state: CreateModelState) => {
-    createVocabulary(graph, state);
+    void (async () => {
+      const models = await createSemanticModels(state);
+      addModelsToGraph(graph, models);
+      const aggregatedView = graph.aggregator.getView();
+      graph.setAggregatorView(aggregatedView);
+    })();
   };
   dialogs.openDialog(createAddModelDialog(onConfirm));
 }
 
 const SGOV_IDENTIFIER = "sgov";
 
-export function createVocabulary(
-  graph: ModelGraphContextType,
+export async function createSemanticModels(
   state: CreateModelState,
-) {
-
-  const addModelFromUrl = (url: string, alias: string) => {
-    void (async () => {
-      const model = await createRdfsModel([url], httpFetch);
-      model.fetchFromPimStore();
-      addModelsToGraph(graph, [model]);
-      model.alias = alias;
-      const aggregatedView = graph.aggregator.getView();
-      graph.setAggregatorView(aggregatedView);
-    })();
-  };
-
-  const addSgov = () => {
-    const model = createSgovModel("https://slovník.gov.cz/sparql", httpFetch);
-    model.allowClass("https://slovník.gov.cz/datový/turistické-cíle/pojem/turistický-cíl").catch(console.error);
-    model.allowClass("https://slovník.gov.cz/veřejný-sektor/pojem/fyzická-osoba").catch(console.error);
-    addModelsToGraph(graph, [model]);
-  };
-
-  const addLocalModel = (alias: string) => {
-    const model = new InMemorySemanticModel();
-    model.setAlias(alias);
-    addModelsToGraph(graph, [model]);
-  };
-
+): Promise<EntityModel[]> {
+  const result: EntityModel[] = [];
   switch (state.activeTab) {
-  case TabType.AddFromUrl:
-    addModelFromUrl(state.modelUrl, state.modelAlias);
-    break;
-  case TabType.AddPredefined:
-    for (const model of state.selectedModels) {
-      if (model.url !== undefined) {
-        addModelFromUrl(model.url, model.alias ?? model.label);
-      } else if (model.identifier === SGOV_IDENTIFIER) {
-        addSgov();
-      } else {
-        LOG.error("Invalid predefined model.", { model });
+    case TabType.AddFromUrl:
+      result.push(await createRdfsSemanticModel(state.modelUrl, state.modelAlias));
+      break;
+    case TabType.AddPredefined:
+      for (const model of state.selectedModels) {
+        if (model.url !== undefined) {
+          result.push(await createRdfsSemanticModel(
+            model.url, model.alias ?? model.label));
+        } else if (model.identifier === SGOV_IDENTIFIER) {
+          result.push(await createCzechSemanticVocabulary());
+        } else {
+          LOG.error("Invalid predefined model.", { model });
+        }
       }
-    }
-    break;
-  case TabType.CreateLocal:
-    addLocalModel(state.modelAlias);
-    break;
+      break;
+    case TabType.CreateLocal:
+      result.push(await createInMemorySemanticModel(state.modelAlias));
+      break;
   }
-
+  return result;
 }
 
 function addModelsToGraph(graph: ModelGraphContextType, models: EntityModel[]) {
   // If there is no visual model, we create a default one.
   if (graph.aggregatorView.getActiveVisualModel() === null) {
-    console.warn("Creating default visual model.")
+    LOG.warn("Creating default visual model.")
     const visualModel = createDefaultWritableVisualModel(models);
     graph.aggregatorView.changeActiveVisualModel(visualModel.getId());
   }
