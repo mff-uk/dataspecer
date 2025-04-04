344af57bce71df203bf6b858e46a81d6ef85427e
RadStr <radek.strycek@seznam.cz>
2025-04-04 21:42:59 +0200

    Refactor method

--- applications/conceptual-model-editor/src/action/add-all-relationships.ts
+++ applications/conceptual-model-editor/src/action/add-all-relationships.ts
@@ -12,7 +12,9 @@ import { ClassesContextType } from "@/context/classes-context";
 import { getDomainAndRangeConceptsIncludingGeneralizations } from "@/util/relationship-utils";
 import { findSourceModelOfEntity } from "@/service/model-service";
 import { VisualModelDiagramNode } from "@/diagram";
-import { createGetVisualEntitiesForRepresentedGlobalWrapper } from "@/util/utils";
+import { createGetVisualEntitiesForRepresentedGlobalWrapper, VisualsForRepresentedWrapper } from "@/util/utils";
+import { SemanticModelGeneralization, SemanticModelRelationship } from "@dataspecer/core-v2/semantic-model/concepts";
+import { SemanticModelRelationshipProfile } from "@dataspecer/core-v2/semantic-model/profile/concepts";
 
 export function addAllRelationshipsForVisualDiagramNodeToVisualModelAction(
   notifications: UseNotificationServiceWriterType,
@@ -55,105 +57,102 @@ export function addAllRelationshipsForVisualDiagramNodeToVisualModelAction(
 
       // If domain is in the visual model diagram node
       if (domain === cclass) {
-        if (range === null) {
-          continue;
-        }
-        // Now just get all the visual representations of the relationship and use them to find visual ends, which are already covered
-        // by the relationship
-        const possibleVisualRanges = getVisualEntitiesForRepresentedGlobal(range);
-        const visualRelationships = visualModel.getVisualEntitiesForRepresented(relationship.id);
-        for (const visualRelationship of visualRelationships) {
-          if (!isVisualRelationship(visualRelationship)) {
-            continue;
-          }
-          if (visualRelationship.visualSource !== visualModelDiagramNode.identifier) {
-            continue;
-          }
-
-          const index = possibleVisualRanges.findIndex(possibleRange => possibleRange.identifier === visualRelationship.visualTarget)
-          if(index === -1) {
-            continue;
-          }
-          possibleVisualRanges.splice(index, 1);
-        }
-        const model = findSourceModelOfEntity(relationship.id, graph.models);
-        if (model === null) {
-          continue;
-        }
-        for (const visualRange of possibleVisualRanges) {
-          if (!isVisualEdgeEnd(visualRange)) {
-            notifications.error("The edge end is not a supported edge end for unknown reason");
-            continue;
-          }
-          if (visualModelDiagramNode.identifier === visualRange.identifier) {
-            // Just skip it, what this would do is create self-loop which represents relationship
-            // which exists inside the diagram node.
-            continue;
-          }
-          const visualRelationship: Omit<VisualRelationship, "identifier" | "type"> = {
-            representedRelationship: relationship.id,
-            model: model?.getId(),
-            waypoints: [],
-            visualSource: visualModelDiagramNode.identifier,
-            visualTarget: visualRange.identifier,
-          }
-
-          visualRelationshipsToAdd.push(visualRelationship);
-        }
+        findRelationshipsForEnd(
+          notifications, graph, visualModel, getVisualEntitiesForRepresentedGlobal,
+          relationship, visualModelDiagramNode.identifier, range, false, visualRelationshipsToAdd);
       }
-
-      // TODO RadStr: Same as the above
       if (range === cclass) {
-        if (domain === null) {
-          continue;
-        }
-        const possibleDomains = getVisualEntitiesForRepresentedGlobal(domain);
-        const visualRelationships = visualModel.getVisualEntitiesForRepresented(relationship.id);
-        for (const visualRelationship of visualRelationships) {
-          if (!isVisualRelationship(visualRelationship)) {
-            continue;
-          }
-          if (visualRelationship.visualTarget !== visualModelDiagramNode.identifier) {
-            continue;
-          }
-
-          const index = possibleDomains.findIndex(possibleDomain => possibleDomain.identifier === visualRelationship.visualSource)
-          if(index === -1) {
-            continue;
-          }
-          possibleDomains.splice(index, 1);
-        }
-
-        const model = findSourceModelOfEntity(relationship.id, graph.models);
-        if (model === null) {
-          continue;
-        }
-        for (const visualDomain of possibleDomains) {
-          const visualRelationship: Omit<VisualRelationship, "identifier" | "type"> = {
-            representedRelationship: relationship.id,
-            model: model?.getId(),
-            waypoints: [],
-            visualSource: visualDomain.identifier,
-            visualTarget: visualModelDiagramNode.identifier,
-          }
-
-          visualRelationshipsToAdd.push(visualRelationship);
-        }
+        findRelationshipsForEnd(
+          notifications, graph, visualModel, getVisualEntitiesForRepresentedGlobal,
+          relationship, visualModelDiagramNode.identifier, domain, true, visualRelationshipsToAdd);
       }
-
     }
   }
 
-  // We don't need visual profile relationships actually, sicne currently we always show them
-  // (when validating model they are shown)
+  // We don't need visual profile relationships actually, since currently we always show them when validating model
   for (const visualRelationshipToAdd of visualRelationshipsToAdd) {
     visualModel.addVisualRelationship(visualRelationshipToAdd)
   }
 
 }
 
-export function addAllRelationshipsForClassToVisualModel(
 
-) {
+/**
+ * End is either domain or range. Returns the relationships which goes from the {@link end} (respectively {@link otherVisualEnd}),
+ * where {@link otherVisualEnd} is the identifier of the visual diagram node.
+ */
+function findRelationshipsForEnd(
+  notifications: UseNotificationServiceWriterType,
+  graph: ModelGraphContextType,
+  visualModel: VisualModel,
+  getVisualEntitiesForRepresentedGlobal: VisualsForRepresentedWrapper,
+  relationship: SemanticModelRelationship | SemanticModelRelationshipProfile | SemanticModelGeneralization,
+  otherVisualEnd: string,
+  end: string | null,
+  isEndSource: boolean,
+  outputArray: Omit<VisualRelationship, "identifier" | "type">[],
+): void {
+  const visualEndPropertyString = isEndSource ? "visualSource" : "visualTarget";
+  const visualOtherEndPropertyString = isEndSource ? "visualTarget" : "visualSource";
 
+  // If domain is in the visual model diagram node
+  if (end === null) {
+    return;
+  }
+  // Now just get all the visual representations for the represented relationship and use them to find visual ends,
+  // which are already covered by the relationship
+  const availableVisualEnds = getVisualEntitiesForRepresentedGlobal(end);
+  const visualRelationships = visualModel.getVisualEntitiesForRepresented(relationship.id);
+  const model = findSourceModelOfEntity(relationship.id, graph.models);
+  if (model === null) {
+    return;
+  }
+
+  for (const visualRelationship of visualRelationships) {
+    if (!isVisualRelationship(visualRelationship)) {
+      continue;
+    }
+    if (visualRelationship[visualOtherEndPropertyString] !== otherVisualEnd) {
+      continue;
+    }
+
+    const index = availableVisualEnds
+      .findIndex(possibleVisualEnd => possibleVisualEnd.identifier === visualRelationship[visualEndPropertyString])
+    if(index === -1) {
+      continue;
+    }
+    availableVisualEnds.splice(index, 1);
+  }
+  for (const availableVisualEnd of availableVisualEnds) {
+    if (!isVisualEdgeEnd(availableVisualEnd)) {
+      notifications.error("The edge end is not a supported edge end for unknown reason");
+      continue;
+    }
+    if (otherVisualEnd === availableVisualEnd.identifier) {
+      // Just skip it, what this would do is create self-loop which represents relationship
+      // which exists inside the diagram node.
+      continue;
+    }
+
+    let visualSource: string;
+    let visualTarget: string;
+    if (isEndSource) {
+      visualSource = availableVisualEnd.identifier;
+      visualTarget = otherVisualEnd;
+    }
+    else {
+      visualSource = otherVisualEnd;
+      visualTarget = availableVisualEnd.identifier;
+    }
+
+    const visualRelationship: Omit<VisualRelationship, "identifier" | "type"> = {
+      representedRelationship: relationship.id,
+      model: model?.getId(),
+      waypoints: [],
+      visualSource,
+      visualTarget,
+    }
+
+    outputArray.push(visualRelationship);
+  }
 }
