09dec8b2f2a63f383ee661d1442681dc4c8580ab
Štěpán Stenchlák <s.stenchlak@gmail.com>
2025-04-06 11:37:51 +0200

    Resolves #713: Images in Respec should be clickable, opening the original image

--- packages/documentation/src/default-configuration.ts
+++ packages/documentation/src/default-configuration.ts
@@ -21,10 +21,12 @@ export const defaultConfiguration: DocumentationConfiguration = {
         <h2>{{#iflng "cs"}}Přehled{{lng}}Overview{{/iflng}}</h2>
 
         {{#each externalArtifacts.svg}}
-          <figure>
-            <img src="{{{URL}}}" alt="{{translate ./label}}" />
-            <figcaption>{{translate ./label}}</figcaption>
-          </figure>
+          <a href="{{{URL}}}">
+            <figure>
+              <img src="{{{URL}}}" alt="{{translate ./label}}" />
+              <figcaption>{{translate ./label}}</figcaption>
+            </figure>
+          </a>
         {{/each}}
       </section>
 
