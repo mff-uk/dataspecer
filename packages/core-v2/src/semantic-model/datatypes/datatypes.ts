8294ad3a6436a11c61b8853fc01f2a76fa88a563
Petr Škoda <skodapetr@gmail.com>
2025-04-08 16:08:14 +0200

    Update datatypes
    Deprecate existing type functions, introduce new type
    detection functions, #803.

--- packages/core-v2/src/semantic-model/datatypes/datatypes.ts
+++ packages/core-v2/src/semantic-model/datatypes/datatypes.ts
@@ -1,4 +1,5 @@
-export const XsdSimpleTypeURIs = [
+
+const XsdSimpleTypeURIs = [
     "http://www.w3.org/2001/XMLSchema#string",
     "http://www.w3.org/2001/XMLSchema#boolean",
     "http://www.w3.org/2001/XMLSchema#decimal",
@@ -17,18 +18,68 @@ export const XsdSimpleTypeURIs = [
     "http://www.w3.org/2001/XMLSchema#QName",
 ];
 
-export const RdfTypeURIs = [
+const RdfTypeURIs = [
     "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString",
 ];
 
-export const RdfsTypeURIs = [
+const RdfsTypeURIs = [
     "http://www.w3.org/2000/01/rdf-schema#Literal",
+];
+
+const SGovTypeURIs = [
+    "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/text",
 ];
 
 export const DataTypeURIs = [
     ...XsdSimpleTypeURIs,
     ...RdfTypeURIs,
     ...RdfsTypeURIs,
-    // other data types
-    "https://ofn.gov.cz/zdroj/základní-datové-typy/2020-07-01/text",
+    ...SGovTypeURIs,
 ];
+
+const PRIMITIVE_TYPES: Set<string> = new Set([
+    ...DataTypeURIs,
+]);
+
+/**
+ * owl:Thing can be primitive as well as complex type.
+ */
+const OWL_THING = "https://www.w3.org/2002/07/owl#Thing";
+
+export function isPrimitiveType(iri: string): boolean {
+    if(iri === OWL_THING) {
+        return true;
+    }
+    return PRIMITIVE_TYPES.has(iri);
+}
+
+export function isComplexType(iri: string): boolean {
+    if(iri === OWL_THING) {
+        return true;
+    }
+    return PRIMITIVE_TYPES.has(iri);
+}
+
+/**
+ * @deprecated
+ */
+export const isXsdSimpleDataType = (uri: string) => {
+    return XsdSimpleTypeURIs.includes(uri);
+};
+
+/**
+ * @deprecated
+ */
+export const isRdfDataType = (uri: string) => {
+    return RdfTypeURIs.includes(uri);
+};
+
+/**
+ * @deprecated
+ */
+export const isDataType = (uri: string | null): uri is string => {
+    if (!uri) {
+        return false;
+    }
+    return isXsdSimpleDataType(uri) || isRdfDataType(uri) || uri === "http://www.w3.org/2000/01/rdf-schema#Literal" || uri.startsWith("https://ofn.gov.cz/zdroj/základní-datové-typy/"); // || isOtherDataType(uri)...
+};
