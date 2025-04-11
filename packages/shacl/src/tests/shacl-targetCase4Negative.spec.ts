b7ec425d2f75b86499fe72967623d9b8f29963bc
Štěpán Stenchlák <s.stenchlak@gmail.com>
2025-04-11 12:07:57 +0200

    Switch from Jest to Vitest
    - CME had a hidden dependency on fail funcion from Jest
    - Shacl and Shex tests interfered with each other

--- packages/shacl/src/tests/shacl-targetCase4Negative.spec.ts
+++ packages/shacl/src/tests/shacl-targetCase4Negative.spec.ts
@@ -9,7 +9,7 @@ const modelCreator = new TargetCase4ModelCreator();
 test('Test SHACL against data - target case #4 NEGATIVE ', async () => {
   const validation = await Support.testFromData(testType, modelCreator);
   expect(validation.conforms).toBe(false);
-  const shape = await fs.readFileSync("src/tests/shapes/" + testType.substring(0,11) + "Shape.ttl",
+  const shape = await fs.readFileSync("src/tests/shapes/" + testType + "Shape.ttl",
     { encoding: 'utf8', flag: 'r' });
   expect(shape).toContain("sh:targetSubjectsOf <http://www.example.org/unikatniPredikat>")
 });
