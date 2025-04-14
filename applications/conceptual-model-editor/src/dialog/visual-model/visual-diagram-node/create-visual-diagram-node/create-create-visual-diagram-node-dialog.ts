739d7199dd8d2660da21087712323f9fb1e1544c
RadStr <radek.strycek@seznam.cz>
2025-04-14 19:43:31 +0200

    Add dialogs related to visual diagram node and move files

---
+++ applications/conceptual-model-editor/src/dialog/visual-model/visual-diagram-node/create-visual-diagram-node/create-create-visual-diagram-node-dialog.ts
@@ -0,0 +1,19 @@
+import { DialogWrapper } from "@/dialog/dialog-api";
+import { EditVisualDiagramNodeDialogState } from "../edit-visual-diagram-node/edit-visual-diagram-node-dialog-controller";
+import { EditVisualDiagramNodeDialog } from "../edit-visual-diagram-node/edit-visual-diagram-node-dialog";
+
+export const createCreateVisualDiagramNodeDialog = (
+  state: EditVisualDiagramNodeDialogState,
+  onConfirm: ((state: EditVisualDiagramNodeDialogState) => void) | null,
+): DialogWrapper<EditVisualDiagramNodeDialogState> => {
+  return {
+    label: "dialog.visual-diagram-node.label-create",
+    component: EditVisualDiagramNodeDialog,
+    state,
+    confirmLabel: "dialog.visual-diagram-node.ok-create",
+    cancelLabel: "dialog.visual-diagram-node.cancel",
+    validate: () => true,
+    onConfirm,
+    onClose: null,
+  };
+};
