c09ca841449dcce9bc54b61aa00c43cfa1fd8a36
unknown <radek.strycek@seznam.cz>
2025-04-25 11:08:53 +0200

    Change component to new version of multilang component

--- applications/conceptual-model-editor/src/dialog/visual-model/visual-diagram-node/edit-visual-diagram-node/edit-visual-diagram-node-dialog.tsx
+++ applications/conceptual-model-editor/src/dialog/visual-model/visual-diagram-node/edit-visual-diagram-node/edit-visual-diagram-node-dialog.tsx
@@ -3,9 +3,9 @@ import {
   EditVisualDiagramNodeDialogState,
   useEditVisualDiagramNodeDialogController
 } from "./edit-visual-diagram-node-dialog-controller";
-import { MultiLanguageInputForLanguageString } from "@/components/input/multi-language-input-4-language-string";
 import { t } from "@/application";
 import { DialogDetailRow } from "@/components/dialog/dialog-detail-row";
+import { InputLanguageString } from "@/dialog/components/input-language-string";
 
 export const EditVisualDiagramNodeDialog = (props: DialogProps<EditVisualDiagramNodeDialogState>) => {
   const controller = useEditVisualDiagramNodeDialogController(props);
@@ -14,11 +14,11 @@ export const EditVisualDiagramNodeDialog = (props: DialogProps<EditVisualDiagram
     <>
       <div className="grid bg-slate-100 md:grid-cols-[25%_75%] md:gap-y-3 md:pl-8 md:pr-16 md:pt-2">
         <DialogDetailRow detailKey={t("create-visual-diagram-node-dialog.model-name")}>
-          <MultiLanguageInputForLanguageString
-            ls={state.representedVisualModelName}
-            setLs={controller.setRepresentedVisualModelName}
-            defaultLang={state.language}
-            inputType="textarea"
+          <InputLanguageString
+            value={state.representedVisualModelName}
+            onChange={controller.setRepresentedVisualModelName}
+            defaultLanguage={state.language}
+            inputType="text"
           />
         </DialogDetailRow>
       </div>
