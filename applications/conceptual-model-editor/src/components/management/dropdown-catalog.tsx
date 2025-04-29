f9fde18f2abbee98862661dd76d169ba5e46eeb8
Petr Škoda <skodapetr@gmail.com>
2025-04-29 16:03:29 +0200

    Update SVG

--- applications/conceptual-model-editor/src/components/management/dropdown-catalog.tsx
+++ applications/conceptual-model-editor/src/components/management/dropdown-catalog.tsx
@@ -147,9 +147,9 @@ const DropDownIcon = () => {
       height="24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <polyline points="6 9 12 15 18 9" />
     </svg>
--- applications/conceptual-model-editor/src/header/view-management.tsx
+++ applications/conceptual-model-editor/src/header/view-management.tsx
@@ -77,9 +77,9 @@ const AddIcon = () => {
       height="24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <line x1="12" y1="5" x2="12" y2="19" />
       <line x1="5" y1="12" x2="19" y2="12" />
