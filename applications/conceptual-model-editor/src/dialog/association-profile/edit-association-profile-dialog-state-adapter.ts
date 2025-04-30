d95001316bce8127be461233a13950fa82ad6d78
RadStr <radek.strycek@seznam.cz>
2025-05-01 00:19:55 +0200

    Create profiles from selection instead of usages

--- applications/conceptual-model-editor/src/dialog/association-profile/edit-association-profile-dialog-state-adapter.ts
+++ applications/conceptual-model-editor/src/dialog/association-profile/edit-association-profile-dialog-state-adapter.ts
@@ -31,3 +31,37 @@ export function associationProfileDialogStateToNewCmeRelationshipProfile(
       item => item.value === state.mandatoryLevel)?.cme ?? null,
   }
 }
+
+export function associationProfileDialogStateToNewCmeRelationshipProfileWithOverridenEnds(
+  state: AssociationProfileDialogState,
+  domain: string,
+  range: string,
+): NewCmeRelationshipProfile {
+  return {
+    model: state.model.identifier,
+    profileOf: state.profiles.map(item => item.identifier),
+    name: state.name,
+    nameSource: state.overrideName ? null :
+      state.nameSource.identifier ?? null,
+    description: state.description,
+    descriptionSource: state.overrideDescription ? null :
+      state.descriptionSource.identifier ?? null,
+    iri: state.iri,
+    usageNote: state.usageNote,
+    usageNoteSource: state.overrideUsageNote ? null :
+      state.usageNoteSource.identifier ?? null,
+    //
+    domain: domain,
+    domainCardinality:
+      state.overrideDomainCardinality ?
+        state.domainCardinality.cardinality : null,
+    range: range,
+    rangeCardinality:
+      state.overrideRangeCardinality ?
+        state.rangeCardinality.cardinality : null,
+    //
+    externalDocumentationUrl: state.externalDocumentationUrl,
+    mandatoryLevel: state.availableMandatoryLevels.find(
+      item => item.value === state.mandatoryLevel)?.cme ?? null,
+  }
+}
