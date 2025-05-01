5551eea08d940023f31e4603a917ea6cc65b0802
RadStr <radek.strycek@seznam.cz>
2025-05-01 20:25:40 +0200

    Refactor

--- applications/conceptual-model-editor/src/dialog/association-profile/edit-association-profile-dialog-state-adapter.ts
+++ applications/conceptual-model-editor/src/dialog/association-profile/edit-association-profile-dialog-state-adapter.ts
@@ -2,34 +2,10 @@ import { NewCmeRelationshipProfile } from "../../dataspecer/cme-model/model";
 import { AssociationProfileDialogState } from "./edit-association-profile-dialog-state";
 
 export function associationProfileDialogStateToNewCmeRelationshipProfile(
-  state: AssociationProfileDialogState): NewCmeRelationshipProfile {
-  return {
-    model: state.model.identifier,
-    profileOf: state.profiles.map(item => item.identifier),
-    name: state.name,
-    nameSource: state.overrideName ? null :
-      state.nameSource.identifier ?? null,
-    description: state.description,
-    descriptionSource: state.overrideDescription ? null :
-      state.descriptionSource.identifier ?? null,
-    iri: state.iri,
-    usageNote: state.usageNote,
-    usageNoteSource: state.overrideUsageNote ? null :
-      state.usageNoteSource.identifier ?? null,
-    //
-    domain: state.domain.identifier,
-    domainCardinality:
-      state.overrideDomainCardinality ?
-        state.domainCardinality.cardinality : null,
-    range: state.range.identifier,
-    rangeCardinality:
-      state.overrideRangeCardinality ?
-        state.rangeCardinality.cardinality : null,
-    //
-    externalDocumentationUrl: state.externalDocumentationUrl,
-    mandatoryLevel: state.availableMandatoryLevels.find(
-      item => item.value === state.mandatoryLevel)?.cme ?? null,
-  }
+  state: AssociationProfileDialogState
+): NewCmeRelationshipProfile {
+  return associationProfileDialogStateToNewCmeRelationshipProfileWithOverridenEnds(
+    state, state.domain.identifier, state.range.identifier);
 }
 
 export function associationProfileDialogStateToNewCmeRelationshipProfileWithOverridenEnds(
